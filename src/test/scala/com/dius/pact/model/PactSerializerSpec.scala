84cd2b5247d9c7d2adbeb7639abc0a46da9e865c
thetrav <the.trav@gmail.com>
2014-01-10 09:23:20 +1100

    load test resource with classloader so tests can run as sub-project

--- src/test/scala/com/dius/pact/model/PactSerializerSpec.scala
+++ src/test/scala/com/dius/pact/model/PactSerializerSpec.scala
@@ -1,14 +1,18 @@
 package com.dius.pact.model
 
 import org.specs2.mutable.Specification
-import java.io.{File, StringWriter, PrintWriter}
-import org.json4s.DefaultFormats
+import java.io.{InputStream, StringWriter, PrintWriter}
 
 class PactSerializerSpec extends Specification {
+
+  def loadTestFile(name: String): InputStream = {
+    this.getClass.getClassLoader.getResourceAsStream(name)
+  }
+
    "PactSerializer" should {
      "serialize pact" in {
        val sw = new StringWriter()
-       val pactString = io.Source.fromFile(new File("src/test/resources/test_pact.json")).mkString
+       val pactString = io.Source.fromInputStream(loadTestFile("test_pact.json")).mkString
 
        Fixtures.pact.serialize(new PrintWriter(sw))
        val json = sw.toString
@@ -16,7 +20,7 @@ class PactSerializerSpec extends Specification {
      }
 
      "deserialize pact" in {
-       val pact = Pact.from(new File("src/test/resources/test_pact.json"))
+       val pact = Pact.from(loadTestFile("test_pact.json"))
        pact must beEqualTo(Fixtures.pact)
      }
    }
