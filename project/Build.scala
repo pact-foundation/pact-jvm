d580344ea02c84419a1bebd1402a9a80b81c64a1
thetrav <the.trav@gmail.com>
2014-01-10 11:11:32 +1100

    publishing

--- project/Build.scala
+++ project/Build.scala
@@ -2,15 +2,59 @@ import sbt._
 import sbt.Keys._
 
 
+object BuildSettings {
+	val publishSettings = Seq(
+		version := "1.0",
+		organization := "com.dius",
+	    publishMavenStyle := true,
+	    // when playing around with a local install of nexus use this:
+        credentials += Credentials("Sonatype Nexus Repository Manager", "localhost", "deployment", "admin123"),
+	    publishTo := Some("releases" at "http://localhost:8081/nexus/content/repositories/releases/"),
+	    // when sonatype comes online use this:
+	    // credentials += Credentials(Path.userHome / ".ivy2" / ".credentials"),
+	    // publishTo := {
+	    //   // val nexus = "https://oss.sonatype.org/"
+	    //   val nexus = "http://localhost:8081/nexus/"
+	    //   if (version.value.trim.endsWith("SNAPSHOT"))
+	    //     Some("snapshots" at nexus + "content/repositories/snapshots")
+	    //   else
+	    //     Some("releases" at nexus + "service/local/staging/deploy/maven2")
+	    // },
+	    pomExtra :=
+	      <url>https://github.com/DiUS/pact-jvm</url>
+	      <licenses>
+	        <license>
+	          <name>Apache 2</name>
+	          <url>http://www.apache.org/licenses/LICENSE-2.0.txt</url>
+	          <distribution>repo</distribution>
+	        </license>
+	      </licenses>
+	      <scm>
+	        <url>https://github.com/DiUS/pact-jvm</url>
+	        <connection>https://github.com/DiUS/pact-jvm.git</connection>
+	      </scm>
+	      <developers>
+	        <developer>
+	          <id>thetrav</id>
+	          <name>Travis Dixon</name>
+	          <email>the.trav@gmail.com</email>
+	        </developer>
+	      </developers>
+  	)
+}
+
 object RootBuild extends Build {
+	import BuildSettings._
 	lazy val pact = Project( 
 		id = "pact-jvm",
-		base = file(".")
-	).aggregate(model, consumer, provider)
+		base = file("."),
+		settings = Defaults.defaultSettings ++ publishSettings)
+		.aggregate(model, consumer, provider)
 
 	lazy val model = Project(
 		id = "pact-model-jvm",
-		base = file("pact-model-jvm"))
+		base = file("pact-model-jvm"),
+		settings = Defaults.defaultSettings ++ publishSettings)
 
 	lazy val consumer = Project( 
 		id = "pact-consumer-jvm",
@@ -18,5 +62,6 @@ object RootBuild extends Build {
 
 	lazy val provider = Project( 
 		id = "pact-provider-jvm",
-		base = file("pact-provider-jvm"))
+		base = file("pact-provider-jvm"),
+		settings = Defaults.defaultSettings ++ publishSettings)
 }
