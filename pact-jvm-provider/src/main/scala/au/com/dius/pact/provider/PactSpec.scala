309b44b97cf7f7540c3c812044f9a270896b35c7
Ken Scambler <ken.scambler@rea-group.com>
2014-04-12 19:08:24 +1000

    Making the stateChangeUrl optional in the provider config

--- pact-jvm-provider/src/main/scala/au/com/dius/pact/provider/PactSpec.scala
+++ pact-jvm-provider/src/main/scala/au/com/dius/pact/provider/PactSpec.scala
@@ -9,6 +9,7 @@ import org.jboss.netty.handler.codec.http._
 import java.nio.charset.Charset
 import com.twitter.finagle.Http
 import com.twitter.util.Await
+import com.twitter.util.Future
 
 
 class PactSpec(config: PactConfiguration, pact: Pact) extends FreeSpec with Assertions {
@@ -18,34 +19,35 @@ class PactSpec(config: PactConfiguration, pact: Pact) extends FreeSpec with Asse
       response
     } catch {
       case NonFatal(e) => throw new RuntimeException(
-s"""Unable to convert response:
-  status: ${response.getStatus.getCode}
-  headers: ${response.headers}
-  body: ${response.getContent.toString(Charset.forName("UTF-8"))}
-for request:
-  $request""")
+        s"""Unable to convert response:
+           |   status: ${response.getStatus.getCode}
+           |   headers: ${response.headers}
+           |   body: ${response.getContent.toString(Charset.forName("UTF-8"))}
+           |for request:
+           |   $request""".stripMargin)
     }
   }
 
   pact.interactions.toList.map { interaction =>
-    s"""pact for consumer ${pact.consumer.name} """ +
-      s"""provider ${pact.provider.name} """ +
-      s"""interaction "${interaction.description}" """ +
-      s"""in state: "${interaction.providerState}" """ in {
+    s"""pact for consumer ${pact.consumer.name} 
+       |provider ${pact.provider.name} 
+       |interaction "${interaction.description}"
+       |in state: "${interaction.providerState}" """.stripMargin in {
 
         val http = Http.newService(s"${config.providerRoot.host}:${config.providerRoot.port}")
 
-        val stateResponse = http(EnterStateRequest(config.stateChangeUrl.url, interaction.providerState))
-
-        val httpResponse = stateResponse.flatMap { _ =>
-          http(ServiceInvokeRequest(config.providerRoot.url, interaction.request))
+        
+        val changeFuture = config.stateChangeUrl match {
+          case Some(stateChangeUrl) => http(EnterStateRequest(stateChangeUrl.url, interaction.providerState))
+          case None => Future()
         }
+        
+        val pactResponseFuture = for {
+          _ <- changeFuture
+          httpResponse <- http(ServiceInvokeRequest(config.providerRoot.url, interaction.request))
+        } yield convert(interaction.request, httpResponse)
 
-        val pactResponse = httpResponse.map { it =>
-          convert(interaction.request, it)
-        }
-
-        val actualResponse = Await.result(pactResponse)
+        val actualResponse = Await.result(pactResponseFuture)
 
         assert(ResponseMatching.matchRules(interaction.response, actualResponse) === MatchFound)
       }
