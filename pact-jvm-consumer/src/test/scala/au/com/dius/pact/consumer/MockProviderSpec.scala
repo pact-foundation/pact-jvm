43423b8946da97215c5a8baaa4006ccaaa26208b
Ronald Holshausen <uglyog@gmail.com>
2014-09-24 16:18:03 +1000

    #55 Netty server is now in package io

--- pact-jvm-consumer/src/test/scala/au/com/dius/pact/consumer/MockProviderSpec.scala
+++ pact-jvm-consumer/src/test/scala/au/com/dius/pact/consumer/MockProviderSpec.scala
@@ -4,6 +4,8 @@ import org.specs2.mutable.Specification
 import au.com.dius.pact.consumer.Fixtures._
 import au.com.dius.pact.model._
 import scala.concurrent.duration.FiniteDuration
+import org.json4s.StringInput
+import org.json4s.jackson
 import org.json4s.JsonDSL._
 import org.json4s.JsonAST.{JField, JString, JObject}
 import scala.concurrent.ExecutionContext
@@ -54,7 +56,6 @@ class MockProviderSpec extends Specification {
       results.matched.size must === (1)
       results.unexpected.size must === (1)
       
-      
 
       def compareRequests(actual: Request, expected: Request) = {
         actual.method must beEqualTo(expected.method)
@@ -65,7 +66,7 @@ class MockProviderSpec extends Specification {
         val expectedHeaders = expected.headers.getOrElse(Map())
         actual.headers.map(_.filter(t => expectedHeaders.contains(t._1))) must beEqualTo(expected.headers)
 
-        actual.body must beEqualTo(expected.body)
+        jackson.parseJson(StringInput(actual.body.get)) must beEqualTo(jackson.parseJson(StringInput(expected.body.get)))
       }
 
       def compare(actual: Interaction, request:Request, response:Response) = {
@@ -82,7 +83,7 @@ class MockProviderSpec extends Specification {
       
       val expectedInvalidResponse = Response(500, Map("Access-Control-Allow-Origin" -> "*"),
         pretty(JObject(JField("error", JString("unexpected request")))), null)
-      
+
       compareRequests(results.unexpected.head, invalidRequest)
       compare(results.matched.head, validRequest, response)
     }
