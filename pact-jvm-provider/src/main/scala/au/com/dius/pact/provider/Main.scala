1094ec54428cffd44c6092d4a01536e3442405e4
Ronald Holshausen <uglyog@gmail.com>
2014-09-24 15:17:40 +1000

    #55 Netty server is now in package io

--- pact-jvm-provider/src/main/scala/au/com/dius/pact/provider/Main.scala
+++ pact-jvm-provider/src/main/scala/au/com/dius/pact/provider/Main.scala
@@ -2,14 +2,13 @@ package au.com.dius.pact.provider
 
 import au.com.dius.pact.model.Pact
 import org.scalatest._
-import org.json4s._
 import org.json4s.jackson.JsonMethods._
 import java.io.File
 
 object Main {
   def loadFiles(pactRoot: File, configFile: File) = {
     implicit val formats = org.json4s.DefaultFormats
-    val config = parse(io.Source.fromFile(configFile).mkString).extract[PactConfiguration]
+    val config = parse(scala.io.Source.fromFile(configFile).mkString).extract[PactConfiguration]
     (config, PactFileSource.loadFiles(pactRoot))
   }
 
