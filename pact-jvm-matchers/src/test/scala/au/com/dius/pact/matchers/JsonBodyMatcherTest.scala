abca29d00b7043850fa633c85f2020eb0f766a1d
Ronald Holshausen <uglyog@gmail.com>
2014-09-29 16:39:29 +1000

    use JObject as the type instead of Map for matching

--- pact-jvm-matchers/src/test/scala/au/com/dius/pact/matchers/JsonBodyMatcherTest.scala
+++ pact-jvm-matchers/src/test/scala/au/com/dius/pact/matchers/JsonBodyMatcherTest.scala
@@ -1,6 +1,6 @@
 package au.com.dius.pact.matchers
 
-import au.com.dius.pact.model.{JsonDiff, Request}
+import au.com.dius.pact.model.{BodyMismatch, JsonDiff, Request}
 import org.junit.runner.RunWith
 import org.specs2.mutable.Specification
 import org.specs2.runner.JUnitRunner
@@ -38,9 +38,17 @@ class JsonBodyMatcherTest extends Specification with AllExpectations {
         matcher.matchBody(expected(), actual(), diffconfig) must beEmpty
       }
 
+      "with equal Maps" in {
+        actualBody = Some("{\"something\": 100}")
+        expectedBody = Some("{\"something\": 100}")
+        matcher.matchBody(expected(), actual(), diffconfig) must beEmpty
+      }
+
     }
 
     "returns a mismatch" should {
+
+      def containMessage(a: List[BodyMismatch], s: String) = new Matcher[List[BodyMismatch]] {}
 
       "when comparing an empty body to anything and we do not allow extra keys" in {
         actualBody = Some("Blah")
@@ -55,13 +63,25 @@ class JsonBodyMatcherTest extends Specification with AllExpectations {
       "when comparing an empty map to a non-empty one" in {
         expectedBody = Some("{}")
         actualBody = Some("{\"something\": 100}")
-        matcher.matchBody(expected(), actual(), diffconfig) must not(beEmpty)
+        val mismatches: List[BodyMismatch] = matcher.matchBody(expected(), actual(), diffconfig)
+        mismatches must not(beEmpty)
+        mismatches must contain((mismatch: BodyMismatch) => mismatch.mismatch must beSome("Expected an empty Map but received JObject(List((something,JInt(100))))"))
       }
 
       "when comparing a map to one with less entries" in {
         expectedBody = Some("{\"something\": 100, \"somethingElse\": 100}")
         actualBody = Some("{\"something\": 100}")
-        matcher.matchBody(expected(), actual(), diffconfig) must not(beEmpty)
+        val mismatches = matcher.matchBody(expected(), actual(), diffconfig)
+        mismatches must not(beEmpty)
+        mismatches must contain((mismatch: BodyMismatch) => mismatch.mismatch must beSome("Expected a Map with at least 2 elements but received 1 elements"))
+      }
+
+      "when the actual body is missing a key" in {
+        expectedBody = Some("{\"something\": 100, \"somethingElse\": 100}")
+        actualBody = Some("{\"something\": 100}")
+        val mismatches = matcher.matchBody(expected(), actual(), diffconfig)
+        mismatches must not(beEmpty)
+        mismatches must contain((mismatch: BodyMismatch) => mismatch.mismatch must beSome("Expected somethingElse=100 but was missing"))
       }
 
     }
