4b0ba836b7b9063bc81fac7d7c4067e5d41aa0a9
Ronald Holshausen <rholshausen@dius.com.au>
2014-09-29 20:12:11 +1000

    bump version

--- project/Build.scala
+++ project/Build.scala
@@ -4,7 +4,7 @@ import com.typesafe.sbt.pgp.PgpKeys._
 
 object BuildSettings {
     val publishSettings = Seq(
-        version := "2.0.8",
+        version := "2.0.9",
         organization := "au.com.dius",
         scalaVersion := "2.10.4",
         crossScalaVersions := Seq("2.10.4", "2.11.2"),
