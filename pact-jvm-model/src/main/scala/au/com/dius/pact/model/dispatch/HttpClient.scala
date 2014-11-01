3ce5f6bb13e6da4a8689e691b75e85f969a88695
Ronald Holshausen <rholshausen@dius.com.au>
2014-11-02 09:14:53 +1100

    #69 - add query parameters to request when using the basic provider

--- pact-jvm-model/src/main/scala/au/com/dius/pact/model/dispatch/HttpClient.scala
+++ pact-jvm-model/src/main/scala/au/com/dius/pact/model/dispatch/HttpClient.scala
@@ -4,10 +4,16 @@ import au.com.dius.pact.model.{Response, Request}
 import scala.concurrent.{ExecutionContext, Future}
 import dispatch.url
 import au.com.dius.pact.model.unfiltered.Conversions
+import com.ning.http.client.FluentStringsMap
 
 object HttpClient {
   def run(request:Request)(implicit executionContext: ExecutionContext):Future[Response] = {
-    val r = url(request.path).underlying(_.setMethod(request.method)) <:< request.headers.getOrElse(Map())
+    val query = request.query.getOrElse("").split("&").map(_.split("=")).foldLeft(new FluentStringsMap()) {
+      (m, a) => m.add(a.head, a.last)
+    }
+    val r = url(request.path).underlying(
+      _.setMethod(request.method).setQueryParameters(query)
+    ) <:< request.headers.getOrElse(Map())
     val httpRequest = request.body.fold(r){ b => r.setBody(b) }
     dispatch.Http(httpRequest).map(Conversions.dispatchResponseToPactResponse)
   }
