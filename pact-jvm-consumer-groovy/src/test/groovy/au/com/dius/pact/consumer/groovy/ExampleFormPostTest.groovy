b907eac1501b779c2e659aa2f6adcc9d39a92ac8
Ronald Holshausen <uglyog@gmail.com>
2015-05-14 09:41:34 +1000

    added an example form post test

---
+++ pact-jvm-consumer-groovy/src/test/groovy/au/com/dius/pact/consumer/groovy/ExampleFormPostTest.groovy
@@ -0,0 +1,33 @@
+package au.com.dius.pact.consumer.groovy
+
+import groovyx.net.http.ContentType
+import groovyx.net.http.HTTPBuilder
+import org.junit.Test
+
+class ExampleFormPostTest {
+
+    @Test
+    void 'handles bodies from form posts'() {
+        def service = new PactBuilder()
+        service {
+            serviceConsumer "Consumer"
+            hasPactWith "Old School Service"
+            port 8000
+
+            uponReceiving('a POST in application/x-www-form-urlencoded')
+            withAttributes(method: 'post', path: '/path',
+                headers: ['Content-Type': 'application/x-www-form-urlencoded'],
+                query: 'number=12345678'
+            )
+            willRespondWith(status: 201, body: 'form posted ok', headers: ['Content-Type': 'text/plain'])
+        }
+
+        service.run {
+            def http = new HTTPBuilder( 'http://localhost:8000' )
+            http.post(path: '/path', body: [number: '12345678'], requestContentType: ContentType.URLENC) { resp ->
+                assert resp.statusLine.statusCode == 201
+            }
+        }
+    }
+
+}
