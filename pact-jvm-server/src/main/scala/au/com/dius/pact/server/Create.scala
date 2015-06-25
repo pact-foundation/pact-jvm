af27ed536815a67437a51aead8f96e94ae683f67
Ronald Holshausen <uglyog@gmail.com>
2015-06-25 17:19:10 +1000

    correct invalid handling of query parameters on pact-jvm-server #128

--- pact-jvm-server/src/main/scala/au/com/dius/pact/server/Create.scala
+++ pact-jvm-server/src/main/scala/au/com/dius/pact/server/Create.scala
@@ -4,7 +4,6 @@ import au.com.dius.pact.consumer.DefaultMockProvider
 import au.com.dius.pact.model.{Pact, Request, Response}
 import com.typesafe.scalalogging.slf4j.StrictLogging
 import org.jboss.netty.handler.codec.http.QueryStringDecoder
-import org.json4s.JValue
 import org.json4s.JsonDSL.{int2jvalue, map2jvalue, string2jvalue}
 import org.json4s.jackson.JsonMethods.pretty
 
@@ -13,7 +12,7 @@ import scala.collection.JavaConversions._
 
 object Create extends StrictLogging {
   
-  def create(state: String, requestBody: JValue, oldState: ServerState): Result = {
+  def create(state: String, requestBody: String, oldState: ServerState): Result = {
     val pact = Pact.from(requestBody)
     val server = DefaultMockProvider.withDefaultConfig()
     val port = server.config.port
@@ -26,16 +25,17 @@ object Create extends StrictLogging {
   def apply(request: Request, oldState: ServerState): Result = {
     def errorJson = pretty(map2jvalue(Map("error" -> "please provide state param and pact body")))
     def clientError = Result(Response(400, Response.CrossSiteHeaders, errorJson, null), oldState)
-    val params = new QueryStringDecoder(request.path).getParameters.toMap
+    val params = new QueryStringDecoder(s"/?${request.query.getOrElse("")}").getParameters.toMap
 
-    logger.error(request.path)
-    logger.error(request.body.toString)
+    logger.debug(s"path=${request.path}")
+    logger.debug(s"query=${request.query.toString}")
+    logger.debug(request.body.toString)
 
     val result = for {
       stateList <- params.get("state")
       state <- stateList.toList.headOption
-      jsonPact <- request.body
-    } yield create(state, jsonPact, oldState)
+      body <- request.body
+    } yield create(state, body, oldState)
 
     result getOrElse clientError
   }
