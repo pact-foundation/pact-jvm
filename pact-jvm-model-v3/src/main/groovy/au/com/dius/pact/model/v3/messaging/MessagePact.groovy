35073f869e710fe31d9b8ae3a487b52ad22d2291
Ronald Holshausen <uglyog@gmail.com>
2015-07-31 14:19:29 +1000

    first simple implementation of writing a v3 message pact

--- pact-jvm-model-v3/src/main/groovy/au/com/dius/pact/model/v3/messaging/MessagePact.groovy
+++ pact-jvm-model-v3/src/main/groovy/au/com/dius/pact/model/v3/messaging/MessagePact.groovy
@@ -2,19 +2,58 @@ package au.com.dius.pact.model.v3.messaging
 
 import au.com.dius.pact.model.Consumer
 import au.com.dius.pact.model.Provider
+import groovy.json.JsonOutput
 import groovy.transform.Canonical
+import groovy.util.logging.Slf4j
+
+import java.util.jar.JarInputStream
 
 /**
  * Pact for a sequences of messages
  */
 @Canonical
+@Slf4j
 class MessagePact {
     Consumer consumer
     Provider provider
     List messages = []
+    Map metadata = ['pact-specification': ['version': '3.0'], 'pact-jvm': ['version': lookupVersion()]]
 
-    @SuppressWarnings('EmptyMethod')
-    void write() {
+    private static String lookupVersion() {
+        def url = MessagePact.protectionDomain.codeSource.location
+        if (url != null) {
+            url.withInputStream { stream ->
+                try {
+                    def jarStream = new JarInputStream(stream)
+                    return jarStream.manifest?.mainAttributes?.getValue('Implementation-Version') ?: ''
+                } catch (e) {
+                    log.warn('Could not load pact-jvm manifest', e)
+                }
+            }
+        }
+        ''
+    }
 
+    void write(String pactDir) {
+        def pactFile = new File(pactDir, "${consumer.name()}-${provider.name()}.json")
+        pactFile.parentFile.mkdirs()
+        pactFile.withWriter { writer ->
+            writer.print JsonOutput.prettyPrint(JsonOutput.toJson(toMap()))
+        }
+    }
+
+    Map toMap() {
+        [
+            consumer: [name: consumer.name()],
+            provider: [name: provider.name()],
+            messages: messages*.toMap(),
+            metadata: metadata
+        ]
+    }
+
+    static Map toMap(object) {
+        object?.properties?.findAll { it.key != 'class' }?.collectEntries {
+            it.value == null || it.value instanceof Serializable ? [it.key, it.value] : [it.key, toMap(it.value)]
+        }
     }
 }
