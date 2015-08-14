e8ba4306760e9d116a608c893c2a7689ad13c723
Ronald Holshausen <uglyog@gmail.com>
2015-08-14 14:58:24 +1000

    loop over all providers defined in the build

--- pact-jvm-provider-lein/src/main/clojure/leiningen/pact_verify.clj
+++ pact-jvm-provider-lein/src/main/clojure/leiningen/pact_verify.clj
@@ -1,7 +1,15 @@
 (ns leiningen.pact-verify
     (:require [clojure.pprint :as pprint]))
 
+(defn- verify-provider [provider consumers]
+       (pprint/pprint provider)
+       (pprint/pprint consumers))
+
 (defn pact-verify
       "Verifies pact files against a provider"
       [project & args]
-      (pprint/pprint "pact-verify called"))
+      (if (contains? project :pact)
+        (doseq [provider (-> project :pact :serviceProviders)]
+               (apply verify-provider provider))
+        (throw (RuntimeException. "No pact definition was found in the project"))))
+
