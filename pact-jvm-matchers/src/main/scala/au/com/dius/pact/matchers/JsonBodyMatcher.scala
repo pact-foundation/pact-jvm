a395d92e85e73bd54ad2a30dd13c777eff58fd3f
Ronald Holshausen <uglyog@gmail.com>
2015-09-23 12:47:02 +1000

    guard against expected lists being empty when using the each-like matchers #166

--- pact-jvm-matchers/src/main/scala/au/com/dius/pact/matchers/JsonBodyMatcher.scala
+++ pact-jvm-matchers/src/main/scala/au/com/dius/pact/matchers/JsonBodyMatcher.scala
@@ -73,26 +73,27 @@ class JsonBodyMatcher extends BodyMatcher  with StrictLogging {
 
   def compareLists(expectedValues: List[Any], actualValues: List[Any], a: Any, b: Any, path: Seq[String],
                    diffConfig: DiffConfig, matchers: Option[Map[String, Map[String, Any]]]): List[BodyMismatch] = {
-    var result = List[BodyMismatch]()
     if (Matchers.matcherDefined(path, matchers)) {
       logger.debug("compareLists: Matcher defined for path " + path)
-      result = Matchers.domatch[BodyMismatch](matchers, path, expectedValues, actualValues,
-        BodyMismatchFactory) ++ compareListContent(expectedValues.padTo(actualValues.length, expectedValues.head),
-        actualValues, path, diffConfig, matchers)
+      var result = Matchers.domatch[BodyMismatch](matchers, path, expectedValues, actualValues, BodyMismatchFactory)
+      if (expectedValues.nonEmpty) {
+        result = result ++ compareListContent(expectedValues.padTo(actualValues.length, expectedValues.head),
+          actualValues, path, diffConfig, matchers)
+      }
+      result
     } else {
       if (expectedValues.isEmpty && actualValues.nonEmpty) {
-        result = List(BodyMismatch(a, b, Some(s"Expected an empty List but received ${valueOf(actualValues)}"),
-          path.mkString(".")))
+        List(BodyMismatch(a, b, Some(s"Expected an empty List but received ${valueOf(actualValues)}"), path.mkString(".")))
       } else {
+        var result = compareListContent(expectedValues, actualValues, path, diffConfig, matchers)
         if (expectedValues.size != actualValues.size) {
           result = result :+ BodyMismatch(a, b,
             Some(s"Expected a List with ${expectedValues.size} elements but received ${actualValues.size} elements"),
             path.mkString("."))
         }
-        result = result ++ compareListContent(expectedValues, actualValues, path, diffConfig, matchers)
+        result
       }
     }
-    result
   }
 
   def compareMaps(expectedValues: Map[String, Any], actualValues: Map[String, Any], a: Any, b: Any, path: Seq[String],
