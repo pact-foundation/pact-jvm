bc54fc5b13ac0c7f973560e25c706a89fbd927c7
Ashley Cambrell <ashley.cambrell@ioof.com.au>
2015-11-26 11:30:19 +1100

    Add pact broker publish task for maven provider

---
+++ pact-jvm-provider-maven/src/main/groovy/au/com/dius/pact/provider/maven/PactPublishMojo.groovy
@@ -0,0 +1,31 @@
+package au.com.dius.pact.provider.maven
+
+import au.com.dius.pact.provider.broker.PactBrokerClient
+import groovy.io.FileType
+import org.apache.maven.plugin.AbstractMojo
+import org.apache.maven.plugin.MojoExecutionException
+import org.apache.maven.plugin.MojoFailureException
+import org.apache.maven.plugins.annotations.Mojo
+import org.apache.maven.plugins.annotations.Parameter
+
+@Mojo(name = 'publish')
+public class PactPublishMojo extends AbstractMojo {
+    @Parameter(required = true, readonly = true, defaultValue = '${project.version}')
+    private String projectVersion
+
+    @Parameter(defaultValue = '${project.build.directory}/pacts')
+    private String pactDirectory
+
+    @Parameter
+    private String pactBrokerUrl
+
+    @Override
+    void execute() throws MojoExecutionException, MojoFailureException {
+        def brokerClient = new PactBrokerClient(pactBrokerUrl)
+        File pactDirectory = pactDirectory as File
+        pactDirectory.eachFileMatch(FileType.FILES, ~/.*\.json/) { pactFile ->
+            print "Publishing ${pactFile.name} ... "
+            println brokerClient.uploadPactFile(pactFile, projectVersion)
+        }
+    }
+}
