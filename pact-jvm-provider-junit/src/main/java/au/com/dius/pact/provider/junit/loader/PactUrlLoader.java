28c84151fe3d9bf778c8658868a27c068132f9e1
Ronald Holshausen <rholshausen@dius.com.au>
2015-12-12 21:11:18 +1100

    jdk 6 fixes

--- pact-jvm-provider-junit/src/main/java/au/com/dius/pact/provider/junit/loader/PactUrlLoader.java
+++ pact-jvm-provider-junit/src/main/java/au/com/dius/pact/provider/junit/loader/PactUrlLoader.java
@@ -2,28 +2,12 @@ package au.com.dius.pact.provider.junit.loader;
 
 import au.com.dius.pact.model.Pact;
 import au.com.dius.pact.model.PactReader;
-import com.fasterxml.jackson.databind.DeserializationFeature;
-import com.fasterxml.jackson.databind.JsonNode;
-import com.fasterxml.jackson.databind.ObjectMapper;
-import com.github.rholder.retry.*;
-import org.apache.commons.io.IOUtils;
-import org.apache.http.HttpHeaders;
-import org.apache.http.HttpResponse;
-import org.apache.http.client.fluent.Request;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 import java.io.IOException;
-import java.text.MessageFormat;
 import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.Collections;
 import java.util.List;
-import java.util.concurrent.ExecutionException;
-import java.util.concurrent.TimeUnit;
-import java.util.stream.StreamSupport;
-
-import static java.util.stream.Collectors.toList;
 
 /**
  * Implementation of {@link PactLoader} that downloads pacts from given urls
