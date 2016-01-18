06bba9d1032d7dfa21107bdc7b190438ff6379cc
Ronald Holshausen <uglyog@gmail.com>
2016-01-18 17:02:58 +1100

    first phase of implementing updated SBT plugin #195

--- pact-jvm-provider-sbt/build.sbt
+++ pact-jvm-provider-sbt/build.sbt
@@ -6,4 +6,9 @@ libraryDependencies ++= Seq(
   "ch.qos.logback" % "logback-classic" % "1.1.3"
 )
 
+scalacOptions ++= Seq("-deprecation", "-feature")
+
 resolvers += Resolver.mavenLocal
+
+/** Console */
+initialCommands in console := "import au.com.dius.pact.provider.sbt._"
--- pact-jvm-provider-sbt/src/main/scala/PactJvmPlugin.scala
+++ pact-jvm-provider-sbt/src/main/scala/PactJvmPlugin.scala
@@ -2,7 +2,7 @@ import au.com.dius.pact.provider.sbtsupport.Main
 import sbt._
 
 object PactJvmPlugin extends Plugin {
-  val verifyPacts = taskKey[Unit]("Verify this provider adheres to the pacts provided by its consumers.")
+  val verifyPacts = taskKey[Unit]("**DEPRECATED** Verify this provider adheres to the pacts provided by its consumers.")
 
   val pactConfig = settingKey[File]("json file containing configuration for the provider test server.")
   val pactRoot = settingKey[File]("root folder for pact files, all .json files in root and sub folders are assumed to be pacts.")
@@ -12,6 +12,9 @@ object PactJvmPlugin extends Plugin {
     pactRoot := file("pacts"),
     verifyPacts := {
       implicit val executionContext = scala.concurrent.ExecutionContext.Implicits.global
+      println
+      println("=== WARNING: verifyPacts is deprecated and is being replaced with an updated task (pactVerify). ===")
+      println
       import Main._
       runPacts(loadFiles(pactRoot.value, pactConfig.value))
     }
