1f7511f3557e7915fec720869f6ff553b466213f
Ronald Holshausen <uglyog@gmail.com>
2016-01-21 11:36:53 +1100

    updated release prep script

--- releasePrep.groovy
+++ releasePrep.groovy
@@ -1,4 +1,6 @@
 #!/usr/bin/env groovy
+@Grab(group = 'com.github.zafarkhaja', module = 'java-semver', version = '0.9.0')
+import com.github.zafarkhaja.semver.Version
 
 def executeOnShell(String command, Closure closure = null) {
   executeOnShell(command, new File(System.properties.'user.dir'), closure)
@@ -21,8 +23,21 @@ def executeOnShell(String command, File workingDir, Closure closure = null) {
   }
 }
 
+void ask(String prompt, String defaultValue = 'Y', Closure cl) {
+  def promptValue = System.console().readLine(prompt + ' ').trim()
+  if (promptValue.empty) {
+    promptValue = defaultValue
+  }
+  if (promptValue.toUpperCase() == 'Y') {
+    cl.call()
+  }
+}
+
 executeOnShell 'git pull'
-executeOnShell './gradlew clean check install'
+
+ask('Execute Build?: [Y]') {
+  executeOnShell './gradlew clean check install'
+}
 
 def projectProps = './gradlew :pact-jvm-consumer_2.11:properties'.execute().text.split('\n').inject([:]) { acc, v ->
   if (v ==~ /\w+: .*/) {
@@ -51,23 +66,47 @@ if (releaseVer.empty) {
   releaseVer = version
 }
 
-def changeLogFile = new File('CHANGELOG.md')
-def changeLogFileLines = changeLogFile.readLines()
-changeLogFile.withPrintWriter() { p ->
-  p.println(changeLogFileLines[0])
+ask('Update Changelog?: [Y]') {
+  def changeLogFile = new File('CHANGELOG.md')
+  def changeLogFileLines = changeLogFile.readLines()
+  changeLogFile.withPrintWriter() { p ->
+    p.println(changeLogFileLines[0])
 
-  p.println()
-  p.println("# $releaseVer - $releaseDesc")
-  p.println()
-  changelog.each {
-    p.println(it)
+    p.println()
+    p.println("# $releaseVer - $releaseDesc")
+    p.println()
+    changelog.each {
+      p.println(it)
+    }
+
+    changeLogFileLines[1..-1].each {
+      p.println(it)
+    }
   }
 
-  changeLogFileLines[1..-1].each {
-    p.println(it)
-  }
+  executeOnShell("git add CHANGELOG.md")
+  executeOnShell("git commit -m 'update changelog for release $releaseVer'")
+  executeOnShell("git status")
 }
 
-executeOnShell("git add CHANGELOG.md")
-executeOnShell("git commit -m 'update changelog for release $releaseVer'")
-executeOnShell("git status")
+ask('Tag and Push commits?: [Y]') {
+  executeOnShell 'git push'
+  executeOnShell("git tag ${releaseVer.replaceAll('\\.', '_')}")
+  executeOnShell 'git push --tags'
+}
+
+ask('Publish artifacts to maven central?: [Y]') {
+  executeOnShell './gradlew clean uploadArchives -S'
+}
+
+def nextVer = Version.valueOf(releaseVer).incrementPatchVersion()
+ask("Bump version to $nextVer?: [Y]") {
+  executeOnShell "sed -i -e \"s/version = '${releaseVer}'/version = '${nextVer}'/\" build.gradle"
+  executeOnShell "sed -i -e \"s/def version = \\\"${releaseVer}\\\"/def version = \\\"${nextVer}\\\"/\" project/Build.scala"
+  executeOnShell("git add build.gradle project/Build.scala")
+  executeOnShell("git diff --cached")
+  ask("Commit and push this change?: [Y]") {
+    executeOnShell("git commit -m 'bump version to $nextVer'")
+    executeOnShell("git push")
+  }
+}
