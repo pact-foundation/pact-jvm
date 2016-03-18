bedae45f3701c8fe546f6817d6f5a3af29b75766
Ronald Holshausen <uglyog@gmail.com>
2016-03-18 16:26:47 +1100

    upgrade groovy to 2.4.6 to fix loss of precision when parsing decimal numbers in JSON #221

--- pact-jvm-model/src/test/groovy/au/com/dius/pact/model/PactWriterSpec.groovy
+++ pact-jvm-model/src/test/groovy/au/com/dius/pact/model/PactWriterSpec.groovy
@@ -18,4 +18,36 @@ class PactWriterSpec extends Specification {
     result.interactions.first().request.query == 'a=b%3Dc%26d'
   }
 
+  def 'should handle body types other than JSON'() {
+    given:
+    def pact = new RequestResponsePact(interactions: [
+      new RequestResponseInteraction(request: new Request(method: 'PUT',
+        body: OptionalBody.body('<?xml version="1.0"><root/>'),
+        headers: ['Content-Type': 'application/xml']),
+        response: new Response(body: OptionalBody.body('Ok, no prob'), headers: ['Content-Type': 'text/plain']))
+    ])
+
+    when:
+    def result = PactWriter.toMap(pact, PactSpecVersion.V2)
+
+    then:
+    result.interactions.first().request.body == '<?xml version="1.0"><root/>'
+    result.interactions.first().response.body == 'Ok, no prob'
+  }
+
+  def 'does not lose the scale for decimal numbers'() {
+    given:
+    def pact = new RequestResponsePact(interactions: [
+      new RequestResponseInteraction(request: new Request(method: 'GET'),
+        response: new Response(body: OptionalBody.body('{"value": 1234.0}'),
+          headers: ['Content-Type': 'application/json']))
+    ])
+
+    when:
+    def result = PactWriter.toMap(pact, PactSpecVersion.V2)
+
+    then:
+    result.interactions.first().response.body.toString() == '{value=1234.0}'
+  }
+
 }
