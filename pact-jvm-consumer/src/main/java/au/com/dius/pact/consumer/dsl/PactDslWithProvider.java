6b8f14bad0033b87c9f48dd98913b9a337480dd6
Alex Soto <asotobu@gmail.com>
2016-10-20 18:09:13 +0200

    [issue #335] adds given method with varargs

--- pact-jvm-consumer/src/main/java/au/com/dius/pact/consumer/dsl/PactDslWithProvider.java
+++ pact-jvm-consumer/src/main/java/au/com/dius/pact/consumer/dsl/PactDslWithProvider.java
@@ -3,6 +3,7 @@ package au.com.dius.pact.consumer.dsl;
 import au.com.dius.pact.consumer.ConsumerPactBuilder;
 import au.com.dius.pact.model.ProviderState;
 
+import java.util.HashMap;
 import java.util.Map;
 
 public class PactDslWithProvider {
@@ -33,6 +34,30 @@ public class PactDslWithProvider {
     public PactDslWithState given(String state, Map<String, Object> params) {
         return new PactDslWithState(consumerPactBuilder, consumerPactBuilder.getConsumerName(), providerName,
           new ProviderState(state, params));
+    }
+
+    /**
+     * Describe the state the provider needs to be in for the pact test to be verified.
+     *
+     * @param firstKey Key of first parameter element
+     * @param firstValue Value of first parameter element
+     * @param paramsKeyValuePair Additional parameters in key-value pairs
+     */
+    public PactDslWithState given(String state, String firstKey, Object firstValue, Object... paramsKeyValuePair) {
+
+        if (paramsKeyValuePair.length % 2 != 0) {
+            throw new IllegalArgumentException("Pair key value should be provided, but there is one key without value.");
+        }
+
+        final Map<String, Object> params = new HashMap<>();
+        params.put(firstKey, firstValue);
+
+        for (int i = 0; i < paramsKeyValuePair.length; i+=2) {
+            params.put(paramsKeyValuePair[i].toString(), paramsKeyValuePair[i+1]);
+        }
+
+        return new PactDslWithState(consumerPactBuilder, consumerPactBuilder.getConsumerName(), providerName,
+                new ProviderState(state, params));
     }
 
     /**
