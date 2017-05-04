b5442783c1532df49c6b8a5b043c73ada750d569
Brent Barker <brentbarker9@gmail.com>
2017-05-04 15:41:10 -0600

    Added ability to use a keystore for the pact mock server #241

--- pact-jvm-consumer/src/main/scala/au/com/dius/pact/consumer/MockProvider.scala
+++ pact-jvm-consumer/src/main/scala/au/com/dius/pact/consumer/MockProvider.scala
@@ -23,6 +23,7 @@ object DefaultMockProvider {
   def apply(config: MockProviderConfig): StatefulMockProvider =
     config match {
       case httpsConfig: MockHttpsProviderConfig => new UnfilteredHttpsMockProvider(httpsConfig)
+      case httpsKeystoreConfig: MockHttpsKeystoreProviderConfig => new UnfilteredHttpsKeystoreMockProvider(httpsKeystoreConfig)
       case _ => new UnfilteredMockProvider(config)
     }
 }
