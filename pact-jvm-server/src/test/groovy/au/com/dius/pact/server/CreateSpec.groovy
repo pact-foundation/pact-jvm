96b45dad4f2c2ae2a8ac00c61f545c71e049e4e9
Brent Barker <bbarke@users.noreply.github.com>
2017-05-13 18:58:47 -0600

    #241 Added ability to use trust store in pact-jvm-server (#425)
    * #241 Added ability to use trust store in pact-jvm-server
    
    * Updated readme
    
    * Updated readme with a little bit more info

--- pact-jvm-server/src/test/groovy/au/com/dius/pact/server/CreateSpec.groovy
+++ pact-jvm-server/src/test/groovy/au/com/dius/pact/server/CreateSpec.groovy
@@ -3,6 +3,8 @@ package au.com.dius.pact.server
 import scala.collection.JavaConversions
 import spock.lang.Specification
 
+import java.nio.file.Paths
+
 @SuppressWarnings(['FactoryMethodName'])
 class CreateSpec extends Specification {
 
@@ -12,14 +14,39 @@ class CreateSpec extends Specification {
 
     when:
     def result = Create.create('test state', pact, new scala.collection.immutable.HashMap(),
-      new Config(4444, 'localhost', false, 20000, 40000, true, 2))
+      new Config(4444, 'localhost', false, 20000, 40000, true,
+              2, '', '', 8444))
 
     then:
     result.response().status == 201
+    result.response().body.value != '{"port": 8444}'
 
     cleanup:
     JavaConversions.asJavaCollection(result.newState().values()).each {
       it.stop()
+    }
+  }
+
+  def 'create starts a new server with the provided pact, using a keystore'() {
+    given:
+    def pact = CreateSpec.getResourceAsStream('/create-pact.json').text
+    def keystorePath = Paths.get('src/test/resources/keystore/pact-jvm-512.jks').toFile().absolutePath
+    def password = 'brentwashere'
+
+    when:
+    def result = Create.create('test state', pact, new scala.collection.immutable.HashMap(),
+            new Config(4444, 'localhost', false, 20000, 40000, true,
+                    2, keystorePath, password, 8444))
+
+    then:
+    result.response().status == 201
+    result.response().body.value == '{"port": 8444}'
+
+    cleanup:
+    if (result != null) {
+      JavaConversions.asJavaCollection(result.newState().values()).each {
+        it.stop()
+      }
     }
   }
 
