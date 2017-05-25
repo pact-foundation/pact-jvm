e7f7bda1c7f1d5fd31437498c9dc7e5ad2ede606
Ronald Holshausen <uglyog@gmail.com>
2017-05-25 13:54:16 +1000

    Enable publishing gradle plugin for V3 branch

--- releasePrep.groovy
+++ releasePrep.groovy
@@ -107,8 +107,7 @@ ask('Tag and Push commits?: [Y]') {
 }
 
 ask('Publish artifacts to maven central?: [Y]') {
-//  executeOnShell './gradlew clean uploadArchives :pact-jvm-provider-gradle_2.11:publishPlugins -S'
-  executeOnShell './gradlew clean uploadArchives'
+  executeOnShell './gradlew clean uploadArchives :pact-jvm-provider-gradle_2.11:publishPlugins -S'
 }
 
 def nextVer = Version.valueOf(releaseVer).incrementPreReleaseVersion()
