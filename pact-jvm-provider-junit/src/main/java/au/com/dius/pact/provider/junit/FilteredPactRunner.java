4962568420bcfdb41ea48c436fb0373ee1bc5bbd
Siddhu Warrier <siwarrie@cisco.com>
2017-06-09 17:10:57 +0100

    Improve quality of filteredPactRunner
    Change-Id: I377c9bb9fd5f52ca345ca1b5ee39f32905f5d4e5

--- pact-jvm-provider-junit/src/main/java/au/com/dius/pact/provider/junit/FilteredPactRunner.java
+++ pact-jvm-provider-junit/src/main/java/au/com/dius/pact/provider/junit/FilteredPactRunner.java
@@ -1,19 +1,15 @@
-package com.cisco.lockhart.pact;
+package au.com.dius.pact.provider.junit;
 
 import java.util.List;
+import java.util.Optional;
 import java.util.Set;
 
 import au.com.dius.pact.model.Pact;
-import au.com.dius.pact.provider.junit.PactRunner;
+import au.com.dius.pact.provider.junit.loader.PactFilter;
 
 import org.junit.runners.model.InitializationError;
 
 import com.google.common.collect.Sets;
-
-/*
- * Copyright (c) Cisco Systems 2017. All rights reserved.
- *
- */
 
 public class FilteredPactRunner extends PactRunner {
 
@@ -23,13 +19,16 @@ public class FilteredPactRunner extends PactRunner {
 
     @Override
     public List<Pact> filterPacts(List<Pact> pacts){
-        PactFilter pactFilter = this.getTestClass().getJavaClass().getAnnotation(PactFilter.class);
-        Set<String> requiredInteractions = Sets.newHashSet(pactFilter.value());
+        Optional<PactFilter> pactFilterOpt = Optional.ofNullable(this.getTestClass().getJavaClass().getAnnotation(PactFilter.class));
 
-        if (requiredInteractions != null && requiredInteractions.size() > 0) {
-            pacts.forEach(pact ->
-                    pact.getInteractions().removeIf(interaction -> !requiredInteractions.contains(interaction.getProviderState())));
-        }
-        return pacts;
+        return pactFilterOpt.map(pactFilter -> {
+            Set<String> requiredInteractions = Sets.newHashSet(pactFilter.value());
+
+            if (requiredInteractions != null && requiredInteractions.size() > 0) {
+                pacts.forEach(pact ->
+                        pact.getInteractions().removeIf(interaction -> !requiredInteractions.contains(interaction.getProviderState())));
+            }
+            return pacts;
+        }).orElse(pacts);
     }
 }
