aabbe5c49f11a1e01758709b810af2cf155f7908
David Mouser <david.mouser@irexchange.com>
2017-06-23 17:09:25 +1000

    Allow setting of a custom MockMvc for provider states that need finer grained control over the test context

---
+++ pact-jvm-provider-spring/src/test/java/au/com/dius/pact/provider/spring/BookTypeArgumentResolver.java
@@ -0,0 +1,26 @@
+package au.com.dius.pact.provider.spring;
+
+import org.springframework.core.MethodParameter;
+import org.springframework.web.bind.support.WebDataBinderFactory;
+import org.springframework.web.context.request.NativeWebRequest;
+import org.springframework.web.method.support.HandlerMethodArgumentResolver;
+import org.springframework.web.method.support.ModelAndViewContainer;
+
+public class BookTypeArgumentResolver implements HandlerMethodArgumentResolver {
+
+	public BookTypeArgumentResolver() {
+	}
+
+  @Override
+  public boolean supportsParameter(MethodParameter parameter) {
+    return parameter.getGenericParameterType().equals(BookType.class);
+  }
+
+  @Override
+  public BookType resolveArgument(MethodParameter methodParameter, ModelAndViewContainer mavContainer,
+                                  NativeWebRequest webRequest, WebDataBinderFactory binderFactory) {
+
+		String type = webRequest.getParameter("type");
+		return new BookType(type);
+	}
+}
--- pact-jvm-provider-spring/src/test/java/au/com/dius/pact/provider/spring/BooksPactProviderTest.java
+++ pact-jvm-provider-spring/src/test/java/au/com/dius/pact/provider/spring/BooksPactProviderTest.java
@@ -11,6 +11,7 @@ import org.junit.runner.RunWith;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.MockitoAnnotations;
+import org.springframework.test.web.servlet.setup.MockMvcBuilders;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -91,6 +92,21 @@ public class BooksPactProviderTest {
     @State("update-book-no-content-type")
     public void updateBookNoContentType() {
         // no setup needed
+    }
+
+    @State("get-books-for-page")
+    public void getBooksForPage() {
+        // Prove that we can provide MockMvcTarget with our own pre-build MockMvc for situations where we need greater control over
+        // how MockMvc is configured; in this instance the request needs a custom argum
+        target.setMockMvc(MockMvcBuilders.standaloneSetup(bookController)
+                                         .setCustomArgumentResolvers(new BookTypeArgumentResolver())
+                                         .build());
+
+        List<Book> bookList = new ArrayList<>();
+        bookList.add(new Book(UUID.randomUUID(), "Bob Jones", true));
+        bookList.add(new Book(UUID.randomUUID(), "Eric Reynolds", true));
+
+        when(bookLogic.getBooks(any(BookType.class))).thenReturn(bookList);
     }
 
     @State("get-books")
