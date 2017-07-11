7c57abe7ad82259c6edfffed28dc017e38caab52
Ronald Holshausen <rholshausen@dius.com.au>
2017-07-11 20:53:47 +1000

    Converted Scala test to Groovy

---
+++ pact-jvm-matchers/src/test/groovy/au/com/dius/pact/model/ResponseMatchingSpec.groovy
@@ -0,0 +1,18 @@
+package au.com.dius.pact.model
+
+import scala.None$
+import scala.Some
+import spock.lang.Specification
+
+class ResponseMatchingSpec extends Specification {
+
+  def 'response matching - match statuses'() {
+    expect:
+    Matching.matchStatus(200, 200) == None$.MODULE$
+  }
+
+  def 'response matching - mismatch statuses'() {
+    expect:
+    Matching.matchStatus(200, 300) == Some.apply(StatusMismatch.apply(200, 300))
+  }
+}
