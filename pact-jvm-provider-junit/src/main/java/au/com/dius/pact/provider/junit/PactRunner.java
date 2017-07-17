pact-foundation/pact-jvm

commit 468429677703620469a7fc13684882db70341edd

Added various pact sources and wired them into the JUnit loaders

pact-jvm-provider-junit/src/main/java/au/com/dius/pact/provider/junit/PactRunner.java
@@ -67,23 +67,24 @@ public class PactRunner extends ParentRunner<InteractionRunner> {
 
         final TestClass testClass = new TestClass(clazz);
 
-        this.child = new ArrayList<>();
-        final List<Pact> pacts;
-        try {
-            pacts = getPactSource(testClass).load(serviceName).stream()
-                  .filter(p -> consumerName == null || p.getConsumer().getName().equals(consumerName))
-                  .collect(Collectors.toList());
-        } catch (final IOException e) {
-            throw new InitializationError(e);
-        }
+      this.child = new ArrayList<>();
+      final List<Pact> pacts;
+      PactLoader pactLoader = getPactSource(testClass);
+      try {
+        pacts = pactLoader.load(serviceName).stream()
+                .filter(p -> consumerName == null || p.getConsumer().getName().equals(consumerName))
+                .collect(Collectors.toList());
+      } catch (final IOException e) {
+        throw new InitializationError(e);
+      }
 
-        if (pacts == null || pacts.isEmpty()) {
-          throw new InitializationError("Did not find any pact files for provider " + providerInfo.value());
-        }
+      if (pacts == null || pacts.isEmpty()) {
+        throw new InitializationError("Did not find any pact files for provider " + providerInfo.value());
+      }
 
-        for (final Pact pact : filterPacts(pacts)) {
-            this.child.add(new InteractionRunner(testClass, pact));
-        }
+      for (final Pact pact : filterPacts(pacts)) {
+        this.child.add(new InteractionRunner(testClass, pact, pactLoader.getPactSource()));
+      }
     }
 
     protected List<Pact> filterPacts(List<Pact> pacts){

pact-jvm-model/src/main/kotlin/au/com/dius/pact/model/PactSource.kt
@@ -1,8 +1,21 @@
 package au.com.dius.pact.model
 
+import java.io.File
+
 /**
  * Represents the source of a Pact
  */
-interface PactSource {
+interface PactSource
 
-}
+data class DirectorySource @JvmOverloads constructor(val dir: File,
+                                                     val pacts: MutableMap<File, Pact> = mutableMapOf()): PactSource
+
+data class PactBrokerSource @JvmOverloads constructor(val host: String,
+                                                      val port: String,
+                                                      val pacts: MutableMap<Consumer, List<Pact>> = mutableMapOf()): PactSource
+
+data class FileSource(val file: File, val pact: Pact): PactSource
+data class UrlSource(val url: String, val pact: Pact): PactSource
+
+data class UrlsSource @JvmOverloads constructor(val url: List<String>,
+                                                val pacts: MutableMap<String, Pact> = mutableMapOf()): PactSource

pact-jvm-provider-junit/src/main/java/au/com/dius/pact/provider/junit/InteractionRunner.java
@@ -1,5 +1,6 @@
 package au.com.dius.pact.provider.junit;
 
+import au.com.dius.pact.model.PactSource;
 import au.com.dius.pact.model.ProviderState;
 import au.com.dius.pact.model.Interaction;
 import au.com.dius.pact.model.Pact;
@@ -7,7 +8,6 @@ import au.com.dius.pact.provider.junit.target.Target;
 import au.com.dius.pact.provider.junit.target.TestClassAwareTarget;
 import au.com.dius.pact.provider.junit.target.TestTarget;
 import org.apache.commons.lang3.ArrayUtils;
-import org.apache.commons.lang3.StringUtils;
 import org.apache.http.HttpRequest;
 import org.junit.After;
 import org.junit.Before;
@@ -44,17 +44,19 @@ import static org.junit.internal.runners.rules.RuleMemberValidator.RULE_VALIDATO
  * Developed with {@link org.junit.runners.BlockJUnit4ClassRunner} in mind
  */
 class InteractionRunner extends Runner {
-    private final TestClass testClass;
-    private final Pact pact;
+  private final TestClass testClass;
+  private final Pact pact;
+  private final PactSource pactSource;
 
-    private final ConcurrentHashMap<Interaction, Description> childDescriptions = new ConcurrentHashMap<>();
+  private final ConcurrentHashMap<Interaction, Description> childDescriptions = new ConcurrentHashMap<>();
 
-    public InteractionRunner(final TestClass testClass, final Pact pact) throws InitializationError {
-        this.testClass = testClass;
-        this.pact = pact;
+  public InteractionRunner(final TestClass testClass, final Pact pact, final PactSource pactSource) throws InitializationError {
+    this.testClass = testClass;
+    this.pact = pact;
+    this.pactSource = pactSource;
 
-        validate();
-    }
+    validate();
+  }
 
     @Override
     public Description getDescription() {
@@ -102,8 +104,7 @@ class InteractionRunner extends Runner {
   }
 
   private void validateTargetRequestFilters(final List<Throwable> errors) {
-    testClass.getAnnotatedMethods(TargetRequestFilter.class)
-      .stream().forEach(method -> {
+    testClass.getAnnotatedMethods(TargetRequestFilter.class).forEach(method -> {
         method.validatePublicVoid(false, errors);
         if (method.getMethod().getParameterTypes().length != 1) {
           errors.add(new Exception("Method " + method.getName() + " should take only a single HttpRequest parameter"));
@@ -114,7 +115,7 @@ class InteractionRunner extends Runner {
   }
 
   protected void validatePublicVoidNoArgMethods(final Class<? extends Annotation> annotation, final boolean isStatic, final List<Throwable> errors) {
-    testClass.getAnnotatedMethods(annotation).stream().forEach(method -> method.validatePublicVoidNoArg(isStatic, errors));
+    testClass.getAnnotatedMethods(annotation).forEach(method -> method.validatePublicVoidNoArg(isStatic, errors));
   }
 
     protected void validateConstructor(final List<Throwable> errors) {
@@ -152,7 +153,7 @@ class InteractionRunner extends Runner {
             final Description description = describeChild(interaction);
             notifier.fireTestStarted(description);
             try {
-                interactionBlock(interaction).evaluate();
+                interactionBlock(interaction, pactSource).evaluate();
             } catch (final Throwable e) {
                 notifier.fireTestFailure(new Failure(description, e));
             } finally {
@@ -165,7 +166,7 @@ class InteractionRunner extends Runner {
         return testClass.getOnlyConstructor().newInstance();
     }
 
-    protected Statement interactionBlock(final Interaction interaction) {
+    protected Statement interactionBlock(final Interaction interaction, final PactSource source) {
         //1. prepare object
         //2. get Target
         //3. run Rule`s
@@ -192,7 +193,7 @@ class InteractionRunner extends Runner {
         Statement statement = new Statement() {
             @Override
             public void evaluate() throws Throwable {
-                target.testInteraction(pact.getConsumer().getName(), interaction);
+                target.testInteraction(pact.getConsumer().getName(), interaction, source);
             }
         };
         statement = withStateChanges(interaction, test, statement);

pact-jvm-provider-junit/src/main/java/au/com/dius/pact/provider/junit/loader/PactBrokerLoader.java
@@ -1,7 +1,7 @@
 package au.com.dius.pact.provider.junit.loader;
 
-import au.com.dius.pact.model.Pact;
-import au.com.dius.pact.model.PactReader;
+import au.com.dius.pact.model.*;
+import au.com.dius.pact.model.PactSource;
 import au.com.dius.pact.provider.ConsumerInfo;
 import au.com.dius.pact.provider.broker.PactBrokerClient;
 import org.apache.commons.lang3.StringUtils;
@@ -31,6 +31,7 @@ public class PactBrokerLoader implements PactLoader {
   private final List<String> pactBrokerTags;
   private boolean failIfNoPactsFound;
   private PactBrokerAuth authentication;
+  private PactBrokerSource pactSource;
 
   public PactBrokerLoader(final String pactBrokerHost, final String pactBrokerPort, final String pactBrokerProtocol) {
       this(pactBrokerHost, pactBrokerPort, pactBrokerProtocol, Collections.singletonList(LATEST));
@@ -43,6 +44,7 @@ public class PactBrokerLoader implements PactLoader {
     this.pactBrokerProtocol = pactBrokerProtocol;
     this.pactBrokerTags = parseTagListExpressions(tags);
     this.failIfNoPactsFound = true;
+    this.pactSource = new PactBrokerSource(this.pactBrokerHost, this.pactBrokerPort);
   }
 
   public PactBrokerLoader(final PactBroker pactBroker) {
@@ -62,6 +64,11 @@ public class PactBrokerLoader implements PactLoader {
       }
     }
     return pacts;
+  }
+
+  @Override
+  public PactSource getPactSource() {
+    return pactSource;
   }
 
   private List<Pact> loadPactsForProvider(final String providerName, final String tag) throws IOException {
@@ -92,7 +99,13 @@ public class PactBrokerLoader implements PactLoader {
   }
 
   Pact loadPact(ConsumerInfo consumer, Map options) {
-    return PactReader.loadPact(options, consumer.getPactFile());
+    Pact pact = PactReader.loadPact(options, consumer.getPactFile());
+    Map<Consumer, List<Pact>> pacts = this.pactSource.getPacts();
+    Consumer pactConsumer = consumer.toPactConsumer();
+    List<Pact> pactList = pacts.getOrDefault(pactConsumer, new ArrayList<>());
+    pactList.add(pact);
+    pacts.put(pactConsumer, pactList);
+    return pact;
   }
 
   PactBrokerClient newPactBrokerClient(URI url) throws URISyntaxException {

pact-jvm-provider-junit/src/main/java/au/com/dius/pact/provider/junit/loader/PactFolderLoader.java
@@ -1,30 +1,33 @@
 package au.com.dius.pact.provider.junit.loader;
 
+import au.com.dius.pact.model.DirectorySource;
 import au.com.dius.pact.model.Pact;
 import au.com.dius.pact.model.PactReader;
+import au.com.dius.pact.model.PactSource;
 
 import java.io.File;
-import java.io.FilenameFilter;
 import java.io.IOException;
 import java.net.URL;
 import java.util.ArrayList;
-import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
+import java.util.stream.Collectors;
 
 /**
  * Out-of-the-box implementation of {@link PactLoader}
  * that loads pacts from either a subfolder of project resource folder or a directory
  */
 public class PactFolderLoader implements PactLoader {
-    private final String path;
+    private final File path;
+    private DirectorySource pactSource;
 
     public PactFolderLoader(final File path) {
-        this(path.getPath());
+      this.path = path;
+      this.pactSource = new DirectorySource(path);
     }
 
     public PactFolderLoader(final String path) {
-      this.path = path;
+      this(new File(path));
     }
 
     /**
@@ -43,46 +46,32 @@ public class PactFolderLoader implements PactLoader {
     public List<Pact> load(final String providerName) throws IOException {
         List<Pact> pacts = new ArrayList<Pact>();
         File pactFolder = resolvePath();
-        File[] files = pactFolder.listFiles(new FilenameFilter() {
-            @Override
-            public boolean accept(File dir, String name) {
-                return name.endsWith(".json");
-            }
-        });
+        File[] files = pactFolder.listFiles((dir, name) -> name.endsWith(".json"));
         if (files != null) {
             for (File file : files) {
                 Pact pact = PactReader.loadPact(file);
                 if (pact.getProvider().getName().equals(providerName)) {
-                    pacts.add(pact);
+                  pacts.add(pact);
+                  this.pactSource.getPacts().put(file, pact);
                 }
             }
         }
         return pacts;
+    }
+
+    @Override
+    public PactSource getPactSource() {
+        return this.pactSource;
     }
 
     public Map<Pact, File> loadPactsWithFiles(final String providerName) throws IOException {
-        Map<Pact, File> pacts = new HashMap<Pact, File>();
-        File pactFolder = resolvePath();
-        File[] files = pactFolder.listFiles(new FilenameFilter() {
-            @Override
-            public boolean accept(File dir, String name) {
-                return name.endsWith(".json");
-            }
-        });
-        if (files != null) {
-            for (File file : files) {
-                Pact pact = PactReader.loadPact(file);
-                if (pact.getProvider().getName().equals(providerName)) {
-                    pacts.put(pact, file);
-                }
-            }
-        }
-        return pacts;
+      return this.pactSource.getPacts().entrySet().stream()
+        .collect(Collectors.toMap(Map.Entry::getValue, Map.Entry::getKey));
     }
 
   private File resolvePath() {
-    File file = new File(path);
-    URL resourcePath = PactFolderLoader.class.getClassLoader().getResource(path);
+    File file = path;
+    URL resourcePath = PactFolderLoader.class.getClassLoader().getResource(path.getPath());
     if (resourcePath != null) {
       file = new File(resourcePath.getPath());
     }

pact-jvm-provider-junit/src/main/java/au/com/dius/pact/provider/junit/loader/PactLoader.java
@@ -1,6 +1,7 @@
 package au.com.dius.pact.provider.junit.loader;
 
-import au.com.dius.pact.model.Pact;
+import au.com.dius.pact.model.*;
+import au.com.dius.pact.model.PactSource;
 
 import java.io.IOException;
 import java.util.List;
@@ -9,11 +10,16 @@ import java.util.List;
  * Encapsulate logic for loading pacts
  */
 public interface PactLoader {
-    /**
-     * Load pacts from appropriate source
-     *
-     * @param providerName name of provider for which pacts will be loaded
-     * @return list of pacts
-     */
-    List<Pact> load(String providerName) throws IOException;
+  /**
+   * Load pacts from appropriate source
+   *
+   * @param providerName name of provider for which pacts will be loaded
+   * @return list of pacts
+   */
+  List<Pact> load(String providerName) throws IOException;
+
+  /**
+   * Returns the source object that the pacts where loaded from
+   */
+  PactSource getPactSource();
 }

pact-jvm-provider-junit/src/main/java/au/com/dius/pact/provider/junit/loader/PactUrlLoader.java
@@ -1,13 +1,16 @@
 package au.com.dius.pact.provider.junit.loader;
 
-import au.com.dius.pact.model.Pact;
-import au.com.dius.pact.model.PactReader;
+import au.com.dius.pact.model.*;
+import au.com.dius.pact.model.PactSource;
+import org.apache.commons.lang3.tuple.Pair;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 import java.io.IOException;
+import java.net.URL;
 import java.util.Arrays;
 import java.util.List;
+import java.util.stream.Collectors;
 
 import static java.util.stream.Collectors.toList;
 
@@ -17,19 +20,29 @@ import static java.util.stream.Collectors.toList;
 public class PactUrlLoader implements PactLoader {
     private static final Logger LOGGER = LoggerFactory.getLogger(PactUrlLoader.class);
     private final String[] urls;
+    private final UrlsSource pactSource;
 
-    public PactUrlLoader(final String[] urls) {
-        this.urls = urls;
-    }
+  public PactUrlLoader(final String[] urls) {
+    this.urls = urls;
+    this.pactSource = new UrlsSource(Arrays.stream(urls).collect(Collectors.toList()));
+  }
 
-    public PactUrlLoader(final PactUrl pactUrl) {
+  public PactUrlLoader(final PactUrl pactUrl) {
         this(pactUrl.urls());
     }
 
-    public List<Pact> load(final String providerName) throws IOException {
-        return Arrays.stream(urls)
-                .map(PactReader::loadPact)
-                .map(obj -> (Pact) obj)
-                .collect(toList());
-    }
+  public List<Pact> load(final String providerName) throws IOException {
+      return Arrays.stream(urls)
+        .map(url -> {
+          Pact pact = PactReader.loadPact(url);
+          this.getPactSource().getPacts().put(url, pact);
+          return pact;
+        })
+        .collect(toList());
+  }
+
+  @Override
+  public UrlsSource getPactSource() {
+    return pactSource;
+  }
 }

pact-jvm-provider-junit/src/main/java/au/com/dius/pact/provider/junit/loader/VersionedPactUrlLoader.java
@@ -1,10 +1,7 @@
 package au.com.dius.pact.provider.junit.loader;
 
-import au.com.dius.pact.model.Pact;
 import com.google.common.annotations.VisibleForTesting;
 
-import java.io.IOException;
-import java.util.List;
 import java.util.Map;
 import java.util.stream.Collectors;
 
@@ -16,11 +13,10 @@ import static java.util.Arrays.stream;
  *
  * @see VersionedPactUrl usage instructions
  */
-public class VersionedPactUrlLoader implements PactLoader {
-    private final String[] urls;
+public class VersionedPactUrlLoader extends PactUrlLoader {
 
     public VersionedPactUrlLoader(String[] urls) {
-        this.urls = urls;
+      super(expandVariables(urls));
     }
 
     @SuppressWarnings("unused")
@@ -28,13 +24,8 @@ public class VersionedPactUrlLoader implements PactLoader {
         this(pactUrl.urls());
     }
 
-    @Override
-    public List<Pact> load(String providerName) throws IOException {
-        return new PactUrlLoader(expandVariables(urls)).load(providerName);
-    }
-
     @VisibleForTesting
-    static String[] expandVariables(String[] urls) throws IOException {
+    static String[] expandVariables(String[] urls) {
         return stream(urls)
                 .map(VersionedPactUrlLoader::expandVariables)
                 .collect(Collectors.toList())

commit b1a0e1acf01f971b4fd77e248a42edead9cc9e82

Fix issue 455
Commit 468429677703620469a7fc13684882db70341edd introduced a feature
regression (see https://github.com/DiUS/pact-jvm/issues/455): it is not
possible anymore to write provider tests before having developed any
consumer expectations.
@PactRunner should accept having no pacts to execute for that matter.

pact-jvm-provider-junit/src/main/java/au/com/dius/pact/provider/junit/PactRunner.java
@@ -78,7 +78,7 @@ public class PactRunner extends ParentRunner<InteractionRunner> {
         throw new InitializationError(e);
       }
 
-      if (pacts == null || pacts.isEmpty()) {
+      if (pacts == null) {
         throw new InitializationError("Did not find any pact files for provider " + providerInfo.value());
       }
 