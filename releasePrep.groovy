e21f9607b76bb887b84fe89d573a153133e803d4
Ronald Holshausen <uglyog@gmail.com>
2017-09-15 14:21:06 +1000

    Need to publish all the jars for the JDK7 version

--- releasePrep.groovy
+++ releasePrep.groovy
@@ -108,7 +108,7 @@ ask('Tag and Push commits?: [Y]') {
 
 ask('Publish artifacts to maven central?: [Y]') {
 //  executeOnShell './gradlew clean uploadArchives :pact-jvm-provider-gradle_2.11:publishPlugins -S'
-  executeOnShell './gradlew clean uploadArchives_2.11 -S'
+  executeOnShell './gradlew clean uploadArchives -S'
 }
 
 def nextVer = Version.valueOf(releaseVer).incrementPatchVersion('jre7.0')
