2fa80e0ea8470b7120fb32bf69fb0c94ae0cc65c
Matt Baumgartner <matthew.baumgartner@philips.com>
2017-09-20 10:10:02 -0400

    added the ability to set MessageConvertors on the MockMvcTarget instance via constuctor or setter

--- pact-jvm-provider-spring/src/test/java/au/com/dius/pact/provider/spring/Book.java
+++ pact-jvm-provider-spring/src/test/java/au/com/dius/pact/provider/spring/Book.java
@@ -1,18 +1,22 @@
 package au.com.dius.pact.provider.spring;
 
+import org.joda.time.DateTime;
+
 import java.util.UUID;
 
 public class Book {
     private UUID id;
     private String author;
     private boolean bestSeller;
+    private DateTime createdOn;
 
     public Book() {}
 
-    public Book(UUID id, String author, boolean bestSeller) {
+    public Book(UUID id, String author, boolean bestSeller, DateTime createdOn) {
         this.id = id;
         this.author = author;
         this.bestSeller = bestSeller;
+        this.createdOn = createdOn;
     }
 
     public UUID getId() {
@@ -38,4 +42,13 @@ public class Book {
     public void setBestSeller(boolean bestSeller) {
         this.bestSeller = bestSeller;
     }
+
+    public DateTime getCreatedOn() {
+        return createdOn;
+    }
+
+    public void setCreatedOn(DateTime createdOn) {
+        this.createdOn = createdOn;
+    }
+
 }
