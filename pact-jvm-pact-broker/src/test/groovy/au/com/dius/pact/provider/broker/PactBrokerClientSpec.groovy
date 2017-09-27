b5c064e868c145ef235e84c8226fab60a6ddb254
Ronald Holshausen <uglyog@gmail.com>
2017-09-27 14:11:35 +1000

    Encode the parameters when uploading a pact, incase the tag has a slash #536

--- pact-jvm-pact-broker/src/test/groovy/au/com/dius/pact/provider/broker/PactBrokerClientSpec.groovy
+++ pact-jvm-pact-broker/src/test/groovy/au/com/dius/pact/provider/broker/PactBrokerClientSpec.groovy
@@ -178,6 +178,35 @@ class PactBrokerClientSpec extends Specification {
     result == 'FAILED! Error'
   }
 
+  def 'encode the provider name, consumer name and tags when uploading a pact'() {
+    given:
+    def halClient = Mock(IHalClient)
+    def client = Spy(PactBrokerClient, constructorArgs: ['baseUrl']) {
+      newHalClient() >> halClient
+    }
+    def tag = 'A/B'
+    pactContents = '''
+      {
+          "provider" : {
+              "name" : "Provider/A"
+          },
+          "consumer" : {
+              "name" : "Foo Consumer/A"
+          },
+          "interactions" : []
+      }
+    '''
+    pactFile.write pactContents
+
+    when:
+    client.uploadPactFile(pactFile, '10.0.0', [tag])
+
+    then:
+    1 * halClient.uploadJson('/pacts/provider/Provider%2FA/consumer/Foo%20Consumer%2FA/version/10.0.0', pactContents, _) >>
+      { args -> args[2].apply('OK', 'OK') }
+    1 * halClient.uploadJson('/pacticipants/Foo%20Consumer%2FA/versions/10.0.0/tags/A%2FB', '')
+  }
+
   @Unroll
   def 'when publishing verification results, return a #result if #reason'() {
     given:
