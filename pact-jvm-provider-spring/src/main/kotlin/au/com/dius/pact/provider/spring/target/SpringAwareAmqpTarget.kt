2b1088c59962ed37aa3d813e5c3af04326538332
Ronald Holshausen <uglyog@gmail.com>
2017-10-13 11:16:08 +1100

    Added a spring context aware target that can look up instances from the spring context

---
+++ pact-jvm-provider-spring/src/main/kotlin/au/com/dius/pact/provider/spring/target/SpringAwareAmqpTarget.kt
@@ -0,0 +1,30 @@
+package au.com.dius.pact.provider.spring.target
+
+import au.com.dius.pact.model.Interaction
+import au.com.dius.pact.provider.ConsumerInfo
+import au.com.dius.pact.provider.ProviderInfo
+import au.com.dius.pact.provider.ProviderVerifier
+import au.com.dius.pact.provider.junit.target.AmqpTarget
+import org.springframework.beans.factory.BeanFactory
+import org.springframework.beans.factory.BeanFactoryAware
+import java.lang.reflect.Method
+import java.util.function.Function
+
+/**
+ * Target for message verification that supports a spring application context. For each annotated method, the owning
+ * bean will be looked up from the application context
+ */
+open class SpringAwareAmqpTarget : AmqpTarget(), BeanFactoryAware {
+  private lateinit var beanFactory: BeanFactory
+
+  override fun setBeanFactory(beanFactory: BeanFactory) {
+    this.beanFactory = beanFactory
+  }
+
+  override fun setupVerifier(interaction: Interaction, provider: ProviderInfo, consumer: ConsumerInfo)
+    : ProviderVerifier {
+    val verifier = super.setupVerifier(interaction, provider, consumer)
+    verifier.providerMethodInstance = Function<Method, Any?> { m -> beanFactory.getBean(m.declaringClass) }
+    return verifier
+  }
+}
