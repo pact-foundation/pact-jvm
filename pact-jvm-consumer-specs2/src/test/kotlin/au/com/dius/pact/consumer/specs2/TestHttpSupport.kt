d7131ecf37942d823fd95c118738ecc7db3cce72
Ronald Holshausen <uglyog@gmail.com>
2017-11-20 15:32:08 +1100

    Fixes after merge from master

--- pact-jvm-consumer-specs2/src/test/kotlin/au/com/dius/pact/consumer/specs2/TestHttpSupport.kt
+++ pact-jvm-consumer-specs2/src/test/kotlin/au/com/dius/pact/consumer/specs2/TestHttpSupport.kt
@@ -13,18 +13,18 @@ object TestHttpSupport {
   fun extractResponseTest(serverUrl: String, path: String, request: Request): Boolean {
     val r = request.copy()
     r.path = "$serverUrl$path"
-    return HttpClient.run(r).thenApply({ response: Response -> response.status == 200 && extractFrom(response.body) }).get()
+    return HttpClient.run(r).thenApply({ response: Response -> response.status == 200 && extractFrom(response.body!!) }).get()
   }
 
   fun simpleGet(serverUrl: String, path: String) =
     HttpClient.run(Request("GET", serverUrl + path))
-      .thenApply({ response -> response.status to response.body.value }).get()
+      .thenApply({ response -> response.status to response.body!!.value }).get()
 
   fun simpleGet(serverUrl: String, path: String, query: String) =
     HttpClient.run(Request("GET", serverUrl + path, PactReader.queryStringToMap(query, true)))
-      .thenApply({ response -> response.status to response.body.value }).get()
+      .thenApply({ response -> response.status to response.body!!.value }).get()
 
   fun options(serverUrl: String, path: String) =
     HttpClient.run(Request("OPTION", serverUrl + path))
-      .thenApply({ response -> Triple(response.status, response.body.orElse(""), response.headers) }).get()
+      .thenApply({ response -> Triple(response.status, response.body!!.orElse(""), response.headers) }).get()
 }
