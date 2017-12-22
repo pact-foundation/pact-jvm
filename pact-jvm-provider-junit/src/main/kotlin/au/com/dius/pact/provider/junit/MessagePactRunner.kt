1b80c7d50b30b283f016667fd5c17b2cdba4b8ee
Ronald Holshausen <uglyog@gmail.com>
2017-12-22 13:55:13 +1100

    fix the filtering of pacts with the Spring test runner #572

--- pact-jvm-provider-junit/src/main/kotlin/au/com/dius/pact/provider/junit/MessagePactRunner.kt
+++ pact-jvm-provider-junit/src/main/kotlin/au/com/dius/pact/provider/junit/MessagePactRunner.kt
@@ -1,5 +1,6 @@
 package au.com.dius.pact.provider.junit
 
+import au.com.dius.pact.model.FilteredPact
 import au.com.dius.pact.model.Pact
 import au.com.dius.pact.model.v3.messaging.MessagePact
 
@@ -8,6 +9,8 @@ import au.com.dius.pact.model.v3.messaging.MessagePact
  */
 open class MessagePactRunner(clazz: Class<*>) : PactRunner(clazz) {
   override fun filterPacts(pacts: List<Pact>): List<Pact> {
-    return pacts.filter { pact -> pact is MessagePact }
+    return super.filterPacts(pacts).filter { pact ->
+      pact is MessagePact || (pact is FilteredPact && pact.pact is MessagePact)
+    }
   }
 }
--- pact-jvm-provider-junit/src/main/kotlin/au/com/dius/pact/provider/junit/RestPactRunner.kt
+++ pact-jvm-provider-junit/src/main/kotlin/au/com/dius/pact/provider/junit/RestPactRunner.kt
@@ -1,10 +1,13 @@
 package au.com.dius.pact.provider.junit
 
+import au.com.dius.pact.model.FilteredPact
 import au.com.dius.pact.model.Pact
 import au.com.dius.pact.model.RequestResponsePact
 
 open class RestPactRunner(clazz: Class<*>) : PactRunner(clazz) {
   override fun filterPacts(pacts: List<Pact>): List<Pact> {
-    return pacts.filter { pact -> pact is RequestResponsePact }
+    return super.filterPacts(pacts).filter { pact ->
+      pact is RequestResponsePact || (pact is FilteredPact && pact.pact is RequestResponsePact)
+    }
   }
 }
