e61648a5e7ee739933ff1e9245ecae8ff3499305
jcreixell <jcreixell@gmail.com>
2018-01-15 18:42:17 +0100

    Upgrade provider SBT plugin to 3.x, Scala 2.12, SBT 1.x
      - Updates `pact-jvm-provider-sbt` plugin to support SBT 1.x, Scala
      2.12.
      - Migrates to [auto plugin](http://www.scala-sbt.org/1.0/docs/Plugins.html) API.
      - See https://github.com/DiUS/pact-jvm/issues/618

--- pact-jvm-provider-sbt/src/main/scala/PactJvmPlugin.scala
+++ pact-jvm-provider-sbt/src/main/scala/PactJvmPlugin.scala
@@ -2,21 +2,24 @@ import au.com.dius.pact.provider.sbtsupport.Main
 import sbt.Keys.TaskStreams
 import sbt._
 
-object PactJvmPlugin extends Plugin {
-  val verifyPacts = taskKey[Unit]("**DEPRECATED** Verify this provider adheres to the pacts provided by its consumers.")
+object PactJvmPlugin extends AutoPlugin {
 
-  val pactConfig = settingKey[File]("json file containing configuration for the provider test server.")
-  val pactRoot = settingKey[File]("root folder for pact files, all .json files in root and sub folders are assumed to be pacts.")
+  object autoImport {
+    lazy val verifyPacts = taskKey[Unit]("**DEPRECATED** Verify this provider adheres to the pacts provided by its consumers.")
 
-  val pactSettings = Seq(
-    pactConfig := file("pact-config.json"),
-    pactRoot := file("pacts"),
-    verifyPacts := {
-      implicit val executionContext = scala.concurrent.ExecutionContext.Implicits.global
-      val s: TaskStreams = Keys.streams.value
-      s.log.warn("=== WARNING: verifyPacts is deprecated and is being replaced with an updated task (pactVerify). ===")
-      import Main._
-      runPacts(loadFiles(pactRoot.value, pactConfig.value))
-    }
-  )
+    lazy val pactConfig = settingKey[File]("json file containing configuration for the provider test server.")
+    lazy val pactRoot = settingKey[File]("root folder for pact files, all .json files in root and sub folders are assumed to be pacts.")
+
+    lazy val pactSettings = Seq(
+      pactConfig := file("pact-config.json"),
+      pactRoot := file("pacts"),
+      verifyPacts := {
+        implicit val executionContext = scala.concurrent.ExecutionContext.Implicits.global
+        val s: TaskStreams = Keys.streams.value
+        s.log.warn("=== WARNING: verifyPacts is deprecated and is being replaced with an updated task (pactVerify). ===")
+        import Main._
+        runPacts(loadFiles(pactRoot.value, pactConfig.value))
+      }
+    )
+  }
 }
