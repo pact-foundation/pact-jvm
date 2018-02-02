95cc457a8322d4c407452daa9a49e04f91bbf742
Chris Pulsinelli <chris@streetcontxt.com>
2018-02-02 11:43:06 -0500

    fixed regex for timezone offset signs

--- pact-jvm-model/src/test/groovy/au/com/dius/pact/model/generators/DateGeneratorSpec.groovy
+++ pact-jvm-model/src/test/groovy/au/com/dius/pact/model/generators/DateGeneratorSpec.groovy
@@ -6,7 +6,7 @@ class DateGeneratorSpec extends Specification {
 
   def 'supports timezones'() {
     expect:
-    new DateGenerator('yyyy-MM-ddZ').generate(null) ==~ /\d{4}-\d{2}-\d{2}(-|\\+)\d+/
+    new DateGenerator('yyyy-MM-ddZ').generate(null) ==~ /\d{4}-\d{2}-\d{2}[-+]\d+/
   }
 
 }
--- pact-jvm-model/src/test/groovy/au/com/dius/pact/model/generators/DateTimeGeneratorSpec.groovy
+++ pact-jvm-model/src/test/groovy/au/com/dius/pact/model/generators/DateTimeGeneratorSpec.groovy
@@ -7,7 +7,7 @@ class DateTimeGeneratorSpec extends Specification {
   def 'supports timezones'() {
     expect:
     new DateTimeGenerator('yyyy-MM-dd\'T\'HH:mm:ssZ').generate(null) ==~
-      /\d{4}-\d{2}-\d{2}T\d{2}:\d{2}:\d{2}(-|\\+)\d+/
+      /\d{4}-\d{2}-\d{2}T\d{2}:\d{2}:\d{2}[-+]\d+/
   }
 
 }
