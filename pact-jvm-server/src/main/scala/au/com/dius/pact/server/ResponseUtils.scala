9743882007155fc51ee992585665d52b33567776
Ronald Holshausen <uglyog@gmail.com>
2018-06-03 16:44:31 +1000

    Removed the server, as it is built on the 2.12 branch

--- pact-jvm-server/src/main/scala/au/com/dius/pact/server/ResponseUtils.scala
+++
@@ -1,5 +0,0 @@
-package au.com.dius.pact.server
-
-object ResponseUtils {
-  val CrossSiteHeaders = Map[String, String]("Access-Control-Allow-Origin" -> "*")
-}
--- pact-jvm-server/src/main/scala/au/com/dius/pact/server/package.scala
+++
@@ -1,8 +0,0 @@
-package au.com.dius.pact
-
-import au.com.dius.pact.consumer.StatefulMockProvider
-import au.com.dius.pact.model.RequestResponseInteraction
-
-package object server {
-  type ServerState = Map[String, StatefulMockProvider[RequestResponseInteraction]]
-}
