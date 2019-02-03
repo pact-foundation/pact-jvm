85dc70dd2465fa17d9c3909b8542caac1666d13e
Ronald Holshausen <uglyog@dius.com.au>
2019-02-03 12:13:13 +1100

    chore: add a test with mutiple providers #820

--- pact-jvm-consumer-junit/src/test/java/au/com/dius/pact/consumer/pactproviderrule/PactProviderWithMultipleFragmentsTest.java
+++ pact-jvm-consumer-junit/src/test/java/au/com/dius/pact/consumer/pactproviderrule/PactProviderWithMultipleFragmentsTest.java
@@ -5,6 +5,7 @@ import au.com.dius.pact.consumer.DefaultResponseValues;
 import au.com.dius.pact.consumer.Pact;
 import au.com.dius.pact.consumer.PactProviderRuleMk2;
 import au.com.dius.pact.consumer.PactVerification;
+import au.com.dius.pact.consumer.PactVerifications;
 import au.com.dius.pact.consumer.dsl.PactDslRequestWithoutPath;
 import au.com.dius.pact.consumer.dsl.PactDslResponse;
 import au.com.dius.pact.consumer.dsl.PactDslWithProvider;
@@ -25,6 +26,9 @@ public class PactProviderWithMultipleFragmentsTest {
     @Rule
     public PactProviderRuleMk2 mockTestProvider = new PactProviderRuleMk2("test_provider", this);
 
+    @Rule
+    public PactProviderRuleMk2 mockTestProvider2 = new PactProviderRuleMk2("test_provider2", this);
+
     @DefaultRequestValues
     public void defaultRequestValues(PactDslRequestWithoutPath request) {
       Map<String, String> headers = new HashMap<String, String>();
@@ -39,7 +43,7 @@ public class PactProviderWithMultipleFragmentsTest {
       response.headers(headers);
     }
 
-    @Pact(consumer="test_consumer")
+    @Pact(consumer="test_consumer", provider = "test_provider")
     public RequestResponsePact createFragment(PactDslWithProvider builder) {
         return builder
             .given("good state")
@@ -59,7 +63,7 @@ public class PactProviderWithMultipleFragmentsTest {
             .toPact();
     }
 
-    @Pact(consumer="test_consumer")
+    @Pact(consumer="test_consumer", provider = "test_provider2")
     public RequestResponsePact createFragment2(PactDslWithProvider builder) {
         return builder
                 .given("good state")
@@ -73,12 +77,12 @@ public class PactProviderWithMultipleFragmentsTest {
     }
 
     @Test
-    @PactVerification(value = "test_provider", fragment = "createFragment2")
+    @PactVerification(value = "test_provider2", fragment = "createFragment2")
     public void runTestWithFragment2() throws IOException {
         Map expectedResponse = new HashMap();
         expectedResponse.put("responsetest", true);
         expectedResponse.put("name", "fred");
-        assertEquals(new ConsumerClient(mockTestProvider.getUrl()).getAsMap("/", ""), expectedResponse);
+        assertEquals(new ConsumerClient(mockTestProvider2.getUrl()).getAsMap("/", ""), expectedResponse);
     }
 
     @Test
@@ -89,5 +93,23 @@ public class PactProviderWithMultipleFragmentsTest {
         expectedResponse.put("responsetest", true);
         expectedResponse.put("name", "harry");
         assertEquals(new ConsumerClient(mockTestProvider.getUrl()).getAsMap("/", ""), expectedResponse);
+    }
+
+    @Test
+    @PactVerifications({
+      @PactVerification(value = "test_provider", fragment = "createFragment"),
+      @PactVerification(value = "test_provider2", fragment = "createFragment2")
+    })
+    public void runTestWithBothFragments() throws IOException {
+        Assert.assertEquals(new ConsumerClient(mockTestProvider.getUrl()).options("/second"), 200);
+        Map expectedResponse = new HashMap();
+        expectedResponse.put("responsetest", true);
+        expectedResponse.put("name", "harry");
+        assertEquals(new ConsumerClient(mockTestProvider.getUrl()).getAsMap("/", ""), expectedResponse);
+
+        expectedResponse = new HashMap();
+        expectedResponse.put("responsetest", true);
+        expectedResponse.put("name", "fred");
+        assertEquals(new ConsumerClient(mockTestProvider2.getUrl()).getAsMap("/", ""), expectedResponse);
     }
 }
