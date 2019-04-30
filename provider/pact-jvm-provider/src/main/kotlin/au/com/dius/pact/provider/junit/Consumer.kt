40d18ff9316805e0b927c0c5f3c7ccfb71a51d73
Ronald Holshausen <uglyog@dius.com.au>
2019-04-30 20:55:27 +1000

    fix: still trying to fix travis

---
+++ provider/pact-jvm-provider/src/main/kotlin/au/com/dius/pact/provider/junit/Consumer.kt
@@ -0,0 +1,17 @@
+package au.com.dius.pact.provider.junit
+
+import java.lang.annotation.Inherited
+import kotlin.annotation.Retention
+
+/**
+ * Used to pass consumer name to Pact runner
+ */
+@Retention(AnnotationRetention.RUNTIME)
+@Target(AnnotationTarget.CLASS, AnnotationTarget.FILE)
+@Inherited
+annotation class Consumer(
+  /**
+   * @return consumer name for pact test running
+   */
+  val value: String
+)
