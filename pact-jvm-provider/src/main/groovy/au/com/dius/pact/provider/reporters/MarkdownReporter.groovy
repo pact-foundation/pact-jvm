136f67c090ffe5257eb05a9b2b3aeaa6c779958f
Ronald Holshausen <uglyog@gmail.com>
2019-06-15 15:07:24 +1000

    fix: MarkdownReporter should append results not overwrite them #893

--- pact-jvm-provider/src/main/groovy/au/com/dius/pact/provider/reporters/MarkdownReporter.groovy
+++ pact-jvm-provider/src/main/groovy/au/com/dius/pact/provider/reporters/MarkdownReporter.groovy
@@ -19,43 +19,41 @@ class MarkdownReporter implements VerifierReporter {
   String name
   File reportDir
   File reportFile
-  PrintWriter writer
   String ext = '.md'
 
   @Override
   void initialise(IProviderInfo provider) {
     reportDir.mkdirs()
     reportFile = new File(reportDir, (provider.name + ext))
-    writer = reportFile.newPrintWriter()
-    writer.println "# $provider.name"
-    writer.println()
-    writer.println '| Description    | Value |'
-    writer.println '| -------------- | ----- |'
-    writer.println "| Date Generated | ${new Date()} |"
-    writer.println "| Pact Version   | ${BasePact.lookupVersion()} |"
-    writer.println()
+    reportFile.append "# $provider.name\n"
+    reportFile.append '\n'
+    reportFile.append '| Description    | Value |\n'
+    reportFile.append '| -------------- | ----- |\n'
+    reportFile.append "| Date Generated | ${new Date()} |\n"
+    reportFile.append "| Pact Version   | ${BasePact.lookupVersion()} |\n"
+    reportFile.append '\n'
   }
 
   @Override
   void finaliseReport() {
-    writer.close()
+
   }
 
   @Override
   void reportVerificationForConsumer(IConsumerInfo consumer, IProviderInfo provider) {
-    writer.println "## Verifying a pact between _${consumer.name}_ and _${provider.name}_"
-    writer.println()
+    reportFile.append "## Verifying a pact between _${consumer.name}_ and _${provider.name}_\n"
+    reportFile.append '\n'
   }
 
   @Override
   void verifyConsumerFromUrl(UrlPactSource pactUrl, IConsumerInfo consumer) {
-    writer.println "From ${pactUrl.description()}"
+    reportFile.append "From ${pactUrl.description()}\n"
   }
 
   @Override
   void verifyConsumerFromFile(PactSource pactFile, IConsumerInfo consumer) {
-    writer.println "From ${pactFile.description()}"
-    writer.println()
+    reportFile.append "From ${pactFile.description()}\n"
+    reportFile.append '\n'
   }
 
   @Override
@@ -69,132 +67,132 @@ class MarkdownReporter implements VerifierReporter {
 
   @Override
   void interactionDescription(Interaction interaction) {
-    writer.println "$interaction.description  "
+    reportFile.append "$interaction.description  \n"
   }
 
   @Override
   void stateForInteraction(String state, IProviderInfo provider, IConsumerInfo consumer, boolean isSetup) {
-    writer.println "Given **$state**  "
+    reportFile.append "Given **$state**  \n"
   }
 
   @Override
   void warnStateChangeIgnored(String state, IProviderInfo IProviderInfo, IConsumerInfo IConsumerInfo) {
-    writer.println '&nbsp;&nbsp;&nbsp;&nbsp;<span style=\'color: yellow\'>WARNING: State Change ignored as there is' +
-      ' no stateChange URL</span>  '
+    reportFile.append '&nbsp;&nbsp;&nbsp;&nbsp;<span style=\'color: yellow\'>WARNING: State Change ignored as there is' +
+      ' no stateChange URL</span>  \n'
   }
 
   @Override
   @SuppressWarnings('ParameterCount')
   void stateChangeRequestFailedWithException(String state, IProviderInfo IProviderInfo, IConsumerInfo IConsumerInfo,
                                              boolean isSetup, Exception e, boolean printStackTrace) {
-    writer.println "&nbsp;&nbsp;&nbsp;&nbsp;<span style='color: red'>State Change Request Failed - $e.message</span>"
-    writer.println()
-    writer.println '```'
-    e.printStackTrace(writer)
-    writer.println '```'
-    writer.println()
+    reportFile.append "&nbsp;&nbsp;&nbsp;&nbsp;<span style='color: red'>State Change Request Failed - $e.message</span>\n"
+    reportFile.append '\n'
+    reportFile.append '```\n'
+    reportFile.withWriterAppend { w -> w.withPrintWriter { e.printStackTrace(it) } }
+    reportFile.append '\n```\n'
+    reportFile.append '\n'
   }
 
   @Override
   void stateChangeRequestFailed(String state, IProviderInfo IProviderInfo, boolean isSetup, String httpStatus) {
-    writer.println "&nbsp;&nbsp;&nbsp;&nbsp;<span style='color: red'>State Change Request Failed - $httpStatus</span>  "
+    reportFile.append "&nbsp;&nbsp;&nbsp;&nbsp;<span style='color: red'>State Change Request Failed - $httpStatus</span>  \n"
   }
 
   @Override
   void warnStateChangeIgnoredDueToInvalidUrl(String state, IProviderInfo IProviderInfo, boolean isSetup,
                                              Object stateChangeHandler) {
-    writer.println '&nbsp;&nbsp;&nbsp;&nbsp;<span style=\'color: yellow\'>WARNING: State Change ignored as there is ' +
-      "no stateChange URL, received `$stateChangeHandler`</span>  "
+    reportFile.append '&nbsp;&nbsp;&nbsp;&nbsp;<span style=\'color: yellow\'>WARNING: State Change ignored as there is ' +
+      "no stateChange URL, received `$stateChangeHandler`</span>  \n"
   }
 
   @Override
   void requestFailed(IProviderInfo IProviderInfo, Interaction interaction, String interactionMessage, Exception e,
                      boolean printStackTrace) {
-    writer.println "&nbsp;&nbsp;&nbsp;&nbsp;<span style='color: red'>Request Failed - $e.message</span>"
-    writer.println()
-    writer.println '```'
-    e.printStackTrace(writer)
-    writer.println '```'
-    writer.println()
+    reportFile.append "&nbsp;&nbsp;&nbsp;&nbsp;<span style='color: red'>Request Failed - $e.message</span>\n"
+    reportFile.append '\n'
+    reportFile.append '```\n'
+    reportFile.withWriterAppend { w -> w.withPrintWriter { e.printStackTrace(it) } }
+    reportFile.append '\n```\n'
+    reportFile.append '\n'
   }
 
   @Override
   void returnsAResponseWhich() {
-    writer.println '&nbsp;&nbsp;returns a response which  '
+    reportFile.append '&nbsp;&nbsp;returns a response which  \n'
   }
 
   @Override
   void statusComparisonOk(int status) {
-    writer.println "&nbsp;&nbsp;&nbsp;&nbsp;has status code **$status** (<span style='color:green'>OK</span>)  "
+    reportFile.append "&nbsp;&nbsp;&nbsp;&nbsp;has status code **$status** (<span style='color:green'>OK</span>)  \n"
   }
 
   @Override
   void statusComparisonFailed(int status, def comparison) {
-    writer.println "&nbsp;&nbsp;&nbsp;&nbsp;has status code **$status** (<span style='color:red'>FAILED</span>)"
-    writer.println()
-    writer.println '```'
+    reportFile.append "&nbsp;&nbsp;&nbsp;&nbsp;has status code **$status** (<span style='color:red'>FAILED</span>)\n"
+    reportFile.append '\n'
+    reportFile.append '```\n'
     if (comparison.hasProperty('message')) {
-      writer.println comparison.message
+      reportFile.append comparison.message
     } else {
-      writer.println comparison
+      reportFile.append comparison
     }
-    writer.println '```'
-    writer.println()
+    reportFile.append '\n```\n'
+    reportFile.append '\n'
   }
 
   @Override
   void includesHeaders() {
-    writer.println '&nbsp;&nbsp;&nbsp;&nbsp;includes headers  '
+    reportFile.append '&nbsp;&nbsp;&nbsp;&nbsp;includes headers  \n'
   }
 
   @Override
   void headerComparisonOk(String key, List<String> value) {
-    writer.println "&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;\"**$key**\" with value \"**$value**\" " +
-      '(<span style=\'color:green\'>OK</span>)  '
+    reportFile.append "&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;\"**$key**\" with value \"**$value**\" " +
+      '(<span style=\'color:green\'>OK</span>)  \n'
   }
 
   @Override
   void headerComparisonFailed(String key, List<String> value, def comparison) {
-    writer.println "&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;\"**$key**\" with value \"**$value**\" " +
-      '(<span style=\'color:red\'>FAILED</span>)  '
-    writer.println()
-    writer.println '```'
-    writer.println comparison
-    writer.println '```'
-    writer.println()
+    reportFile.append "&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;\"**$key**\" with value \"**$value**\" " +
+      '(<span style=\'color:red\'>FAILED</span>)  \n'
+    reportFile.append '\n'
+    reportFile.append '```\n'
+    reportFile.append comparison
+    reportFile.append '\n```\n'
+    reportFile.append '\n'
   }
 
   @Override
   void bodyComparisonOk() {
-    writer.println "&nbsp;&nbsp;&nbsp;&nbsp;has a matching body (<span style='color:green'>OK</span>)  "
+    reportFile.append "&nbsp;&nbsp;&nbsp;&nbsp;has a matching body (<span style='color:green'>OK</span>)  \n"
   }
 
   @Override
   void bodyComparisonFailed(def comparison) {
-    writer.println "&nbsp;&nbsp;&nbsp;&nbsp;has a matching body (<span style='color:red'>FAILED</span>)  "
-    writer.println()
-    writer.println '| Path | Failure |'
-    writer.println '| ---- | ------- |'
+    reportFile.append "&nbsp;&nbsp;&nbsp;&nbsp;has a matching body (<span style='color:red'>FAILED</span>)  \n"
+    reportFile.append '\n'
+    reportFile.append '| Path | Failure |\n'
+    reportFile.append '| ---- | ------- |\n'
     if (comparison instanceof String) {
-      writer.println "|\$|$comparison|"
+      reportFile.append "|\$|$comparison|\n"
     } else if (comparison.comparison instanceof Map) {
-      writer.println comparison.comparison.collect { "|$it.key|${it.value*.mismatch.join('; ')}|" }.join('\n')
+      reportFile.append comparison.comparison.collect { "|$it.key|${it.value*.mismatch.join('; ')}|" }.join('\n')
     } else {
-      writer.println "|\$|$comparison.comparison|"
+      reportFile.append "|\$|$comparison.comparison|"
     }
-    writer.println()
+    reportFile.append '\n\n'
     if (comparison.diff) {
-      writer.println 'Diff:'
-      writer.println()
+      reportFile.append 'Diff:\n'
+      reportFile.append '\n'
       renderDiff comparison.diff
-      writer.println()
+      reportFile.append '\n\n'
     }
   }
 
   void renderDiff(def diff) {
-    writer.println '```diff'
-    writer.println diff.join('\n')
-    writer.println '```'
+    reportFile.append '```diff\n'
+    reportFile.append diff.join('\n')
+    reportFile.append '\n```\n'
   }
 
   @Override
@@ -202,17 +200,17 @@ class MarkdownReporter implements VerifierReporter {
 
   @Override
   void verificationFailed(Interaction interaction, Exception e, boolean printStackTrace) {
-    writer.println "&nbsp;&nbsp;&nbsp;&nbsp;<span style='color: red'>Verification Failed - $e.message</span>"
-    writer.println()
-    writer.println '```'
-    e.printStackTrace(writer)
-    writer.println '```'
-    writer.println()
+    reportFile.append "&nbsp;&nbsp;&nbsp;&nbsp;<span style='color: red'>Verification Failed - $e.message</span>\n"
+    reportFile.append '\n'
+    reportFile.append '```\n'
+    reportFile.withWriterAppend { w -> w.withPrintWriter { e.printStackTrace(it) } }
+    reportFile.append '\n```\n'
+    reportFile.append '\n'
   }
 
   @Override
   void generatesAMessageWhich() {
-    writer.println '&nbsp;&nbsp;generates a message which  '
+    reportFile.append '&nbsp;&nbsp;generates a message which  \n'
   }
 
   @Override
@@ -220,28 +218,28 @@ class MarkdownReporter implements VerifierReporter {
 
   @Override
   void metadataComparisonFailed(String key, def value, def comparison) {
-    writer.println "&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;\"**$key**\" with value \"**$value**\" " +
-      '(<span style=\'color:red\'>FAILED</span>)  '
-    writer.println()
-    writer.println '```'
-    writer.println comparison
-    writer.println '```'
-    writer.println()
+    reportFile.append "&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;\"**$key**\" with value \"**$value**\" " +
+      '(<span style=\'color:red\'>FAILED</span>)  \n'
+    reportFile.append '\n'
+    reportFile.append '```\n'
+    reportFile.append comparison
+    reportFile.append '\n```\n'
+    reportFile.append '\n'
   }
 
   @Override
   void includesMetadata() {
-    writer.println '&nbsp;&nbsp;&nbsp;&nbsp;includes metadata  '
+    reportFile.append '&nbsp;&nbsp;&nbsp;&nbsp;includes metadata  \n'
   }
 
   @Override
   void metadataComparisonOk(@NotNull String key, @Nullable Object value) {
-    writer.println "&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;\"**$key**\" with value \"**$value**\" " +
-      '(<span style=\'color:green\'>OK</span>)  '
+    reportFile.append "&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;\"**$key**\" with value \"**$value**\" " +
+      '(<span style=\'color:green\'>OK</span>)  \n'
   }
 
   @Override
   void metadataComparisonOk() {
-    writer.println "&nbsp;&nbsp;&nbsp;&nbsp;has matching metadata (<span style='color:green'>OK</span>)"
+    reportFile.append "&nbsp;&nbsp;&nbsp;&nbsp;has matching metadata (<span style='color:green'>OK</span>)\n"
   }
 }
