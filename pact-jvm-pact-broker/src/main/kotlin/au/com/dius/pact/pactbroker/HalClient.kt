63542f369608517554249b82ffc8580ff0bc6799
Ronald Holshausen <uglyog@gmail.com>
2019-07-20 17:08:26 +1000

    fix: preserve the scheme when adding premtive auth #902

--- pact-jvm-pact-broker/src/main/kotlin/au/com/dius/pact/pactbroker/HalClient.kt
+++ pact-jvm-pact-broker/src/main/kotlin/au/com/dius/pact/pactbroker/HalClient.kt
@@ -201,7 +201,7 @@ abstract class HalClientBase @JvmOverloads constructor(
               builder.setDefaultCredentialsProvider(credsProvider)
 
               if (System.getProperty(PREEMPTIVE_AUTHENTICATION) == "true") {
-                val targetHost = HttpHost(uri.host, uri.port, "http")
+                val targetHost = HttpHost(uri.host, uri.port, uri.scheme)
                 logger.warn { "Using preemptive basic authentication with the pact broker at $targetHost" }
                 val authCache = BasicAuthCache()
                 val basicAuth = BasicScheme()
