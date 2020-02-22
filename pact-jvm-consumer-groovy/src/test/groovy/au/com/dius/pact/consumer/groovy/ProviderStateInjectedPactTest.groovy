0be41423ef09cbe1f99a367c93d15f3bb93834d3
Ronald Holshausen <uglyog@gmail.com>
2020-02-22 16:16:04 +1100

    fix: fromProviderState on path doesnt work in pact-jvm-consumer-groovy #1022
    (cherry picked from commit 6bf77f3cf95e77577df1884473a9881abc27d1ad)

--- pact-jvm-consumer-groovy/src/test/groovy/au/com/dius/pact/consumer/groovy/ProviderStateInjectedPactTest.groovy
+++ pact-jvm-consumer-groovy/src/test/groovy/au/com/dius/pact/consumer/groovy/ProviderStateInjectedPactTest.groovy
@@ -21,7 +21,9 @@ class ProviderStateInjectedPactTest {
     service {
       given('a provider state with injectable values', [valueA: 'A', valueB: 100])
       uponReceiving('a request')
-      withAttributes(method: 'POST', path: '/values')
+      withAttributes(method: 'POST', path: fromProviderState(
+        '/shoppingCart/v2.0/shoppingCart/${shoppingcartId}',
+        '/shoppingCart/v2.0/shoppingCart/ShoppingCart_05540051-1155-4557-8080-008a802200aa'))
       withBody {
         userName 'Test'
         userClass 'Shoddy'
@@ -36,7 +38,7 @@ class ProviderStateInjectedPactTest {
 
     PactVerificationResult result = service.runTest { mockServer, context ->
       def client = new RESTClient(mockServer.url, 'application/json')
-      def response = client.post(path: '/values', body: [userName: 'Test', userClass: 'Shoddy'])
+      def response = client.post(path: '/shoppingCart/v2.0/shoppingCart/ShoppingCart_05540051-1155-4557-8080-008a802200aa', body: [userName: 'Test', userClass: 'Shoddy'])
 
       assert response.status == 200
       assert response.data == [userName: 'Test', userId: 100]
@@ -46,7 +48,14 @@ class ProviderStateInjectedPactTest {
     def pactFile = new File("${PactConsumerConfig.pactDirectory}/V3Consumer-ProviderStateService.json")
     def json = new JsonSlurper().parse(pactFile)
     assert json.metadata.pactSpecification.version == '3.0.0'
-    def generators = json.interactions.first().response.generators
+    def interaction = json.interactions.first()
+    assert interaction.request.path ==
+      '/shoppingCart/v2.0/shoppingCart/ShoppingCart_05540051-1155-4557-8080-008a802200aa'
+    def generators = interaction.request.generators
+    assert generators == [
+      path: [type: 'ProviderState', expression: '/shoppingCart/v2.0/shoppingCart/${shoppingcartId}']
+    ]
+    generators = interaction.response.generators
     assert generators == [
       body: ['$.userId': [type: 'ProviderState', expression: 'userId']],
       header: [LOCATION: [type: 'ProviderState', expression: 'http://server/users/${userId}']]
