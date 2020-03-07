436a5e85680778dd5d4514b6d4071c41e61e3db7
Ronald Holshausen <uglyog@gmail.com>
2020-03-07 16:12:59 +1100

    Revert "chore: remove system property fallback from SpringEnvironmentResolver #1023"
    This reverts commit 0312084f6e153a3962231f0d130f8ac3bcda7058.

--- provider/pact-jvm-provider-spring/src/main/kotlin/au/com/dius/pact/provider/spring/SpringEnvironmentResolver.kt
+++ provider/pact-jvm-provider-spring/src/main/kotlin/au/com/dius/pact/provider/spring/SpringEnvironmentResolver.kt
@@ -1,12 +1,13 @@
 package au.com.dius.pact.provider.spring
 
+import au.com.dius.pact.core.support.expressions.SystemPropertyResolver
 import au.com.dius.pact.core.support.expressions.ValueResolver
-import au.com.dius.pact.core.support.isNotEmpty
 import org.springframework.core.env.Environment
 
 class SpringEnvironmentResolver(private val environment: Environment) : ValueResolver {
   override fun resolveValue(property: String?): String? {
-    return if (property.isNotEmpty()) environment.getProperty(property) else null
+    val tuple = SystemPropertyResolver.PropertyValueTuple(property).invoke()
+    return environment.getProperty(tuple.propertyName, tuple.defaultValue)
   }
 
   override fun propertyDefined(property: String) = environment.containsProperty(property)
