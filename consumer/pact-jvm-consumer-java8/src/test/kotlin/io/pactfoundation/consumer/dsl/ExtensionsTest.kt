045499f343736c14ebf570d330e39f36016e60a2
Guido Pio Mariotti <guidopio.mariotti10+dev@gmail.com>
2020-03-13 16:30:41 +0100

    feat: create Kotlin friendly extension for arrays

--- consumer/pact-jvm-consumer-java8/src/test/kotlin/io/pactfoundation/consumer/dsl/ExtensionsTest.kt
+++ consumer/pact-jvm-consumer-java8/src/test/kotlin/io/pactfoundation/consumer/dsl/ExtensionsTest.kt
@@ -1,19 +1,41 @@
 package io.pactfoundation.consumer.dsl
 
+import org.hamcrest.CoreMatchers.equalTo
+import org.junit.Assert.assertThat
 import org.junit.jupiter.api.Test
 
 class ExtensionsTest {
-  @Test
-  fun `can use LambdaDslJsonArray#newObject`() {
-    newJsonArray { newObject { stringType("foo") } }
-  }
+    @Test
+    fun `can use Kotlin DSL to create a Json Array`() {
+        val expectedJson = """[
+        |{"key":"value"},
+        |{"key_1":"value_1"}
+        |]""".trimMargin().replace("\n", "")
 
-  @Test
-  fun `can use LambdaDslObject#newObject`() {
-    newJsonObject {
-      newObject("object") {
-        stringType("field")
-      }
+        val actualJson = newJsonArray {
+            newObject { stringValue("key", "value") }
+            newObject { stringValue("key_1", "value_1") }
+        }.body.toString()
+
+        assertThat(actualJson, equalTo(expectedJson))
     }
-  }
+
+    @Test
+    fun `can use Kotlin DSL to create a Json`() {
+        val expectedJson = """{
+        |"array":[{"key":"value"}],
+        |"object":{"property":"value"}
+        |}""".trimMargin().replace("\n", "")
+
+        val actualJson = newJsonObject {
+            newArray("array") {
+                newObject { stringValue("key", "value") }
+            }
+            newObject("object") {
+                stringValue("property", "value")
+            }
+        }.body.toString()
+
+        assertThat(actualJson, equalTo(expectedJson))
+    }
 }
