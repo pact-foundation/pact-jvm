c9ef690c44ecf7acc48f4920d46b0b1238b17fdc
Ronald Holshausen <uglyog@gmail.com>
2020-03-21 13:23:06 +1100

    test: add tests for generated values with XML within JSON #1031

--- core/model/src/test/groovy/au/com/dius/pact/core/model/PactReaderSpec.groovy
+++ core/model/src/test/groovy/au/com/dius/pact/core/model/PactReaderSpec.groovy
@@ -7,8 +7,12 @@ import com.amazonaws.services.s3.model.S3Object
 import com.amazonaws.services.s3.model.S3ObjectInputStream
 import com.google.gson.JsonParser
 import org.apache.http.impl.client.BasicCredentialsProvider
+import spock.lang.Issue
 import spock.lang.Specification
 import spock.lang.Unroll
+import spock.util.environment.RestoreSystemProperties
+
+import static au.com.dius.pact.core.model.generators.Category.BODY
 
 @SuppressWarnings('DuplicateMapLiteral')
 class PactReaderSpec extends Specification {
@@ -313,7 +317,7 @@ class PactReaderSpec extends Specification {
   @Unroll
   def 'determining pact spec version'() {
     expect:
-    DefaultPactReader.INSTANCE.determineSpecVersion(new JsonParser().parse(json)) == version
+    DefaultPactReader.INSTANCE.determineSpecVersion(JsonParser.parseString(json)) == version
 
     where:
 
@@ -327,6 +331,22 @@ class PactReaderSpec extends Specification {
     '{"metadata":{"pactSpecification":{"version":"3.0"}}}'    | '3.0.0'
     '{"metadata":{"pact-specification":{"version":"1.2.3"}}}' | '1.2.3'
 
+  }
+
+  @Issue('#1031')
+  def 'handle encoded values in the pact file'() {
+    given:
+    def pactUrl = PactReaderSpec.classLoader.getResource('encoded-values-pact.json')
+
+    when:
+    def pact = DefaultPactReader.INSTANCE.loadPact(pactUrl)
+
+    then:
+    pact instanceof RequestResponsePact
+    pact.interactions[0].request.body.valueAsString() ==
+      '{"entityName":"mock-name","xml":"<?xml version=\\"1.0\\" encoding=\\"UTF-8\\"?>\\n"}'
+    pact.interactions[0].request.generators.categories[BODY]['$'].expression ==
+      '{\n  "entityName": "${eName}",\n  "xml": "<?xml version=\\"1.0\\" encoding=\\"UTF-8\\"?>\\n"\n}'
   }
 
 }
