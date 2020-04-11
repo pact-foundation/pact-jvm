d1b54ff3b42ad7ec9365570aa4c9fd6b8aeb2b68
Ronald Holshausen <uglyog@gmail.com>
2020-04-11 16:01:10 +1000

    chore: update KTor ro 1.3.2
    (cherry picked from commit 468392fb0c560bbbb2afd6bcff5e6807dba479fa)

--- consumer/pact-jvm-consumer/src/main/kotlin/au/com/dius/pact/consumer/model/MockHttpsProviderConfig.kt
+++ consumer/pact-jvm-consumer/src/main/kotlin/au/com/dius/pact/consumer/model/MockHttpsProviderConfig.kt
@@ -1,7 +1,6 @@
 package au.com.dius.pact.consumer.model
 
 import au.com.dius.pact.core.model.PactSpecVersion
-import io.ktor.network.tls.certificates.generateCertificate
 import java.io.File
 import java.security.KeyStore
 
@@ -24,7 +23,7 @@ class MockHttpsProviderConfig @JvmOverloads constructor(
     @JvmOverloads
     fun httpsConfig(hostname: String = LOCALHOST, port: Int = 0, pactVersion: PactSpecVersion = PactSpecVersion.V3): MockHttpsProviderConfig {
       val jksFile = File.createTempFile("PactTest", ".jks")
-      val keystore = generateCertificate(jksFile, "SHA1withRSA", "PactTest", "changeit", "changeit", 1024)
+      val keystore = io.ktor.network.tls.certificates.generateCertificate(jksFile, "SHA1withRSA", "PactTest", "changeit", "changeit", 1024)
       return MockHttpsProviderConfig(hostname, port, pactVersion, keystore, "PactTest", "changeit", "changeit",
         MockServerImplementation.KTorServer)
     }
