a99d58abfc435915e8798f82f7e21629e1c0af21
Ronald Holshausen <uglyog@gmail.com>
2020-05-09 17:34:05 +1000

    chore: setup a test suite project to test different versions of Gradle

---
+++ provider/pact-jvm-provider-gradle/tests_suits/test_provider/src/ratpack/Ratpack.groovy
@@ -0,0 +1,49 @@
+import ratpack.groovy.template.MarkupTemplateModule
+
+import static ratpack.groovy.Groovy.groovyMarkupTemplate
+import static ratpack.groovy.Groovy.ratpack
+import org.slf4j.Logger
+import org.slf4j.LoggerFactory
+import ratpack.handling.RequestLogger
+
+final Logger logger = LoggerFactory.getLogger(Ratpack.class)
+
+ratpack {
+  bindings {
+    module MarkupTemplateModule
+  }
+
+  handlers {
+    all RequestLogger.ncsa(logger)
+
+    get {
+      render groovyMarkupTemplate("index.gtpl", title: "My Ratpack App")
+    }
+
+    get('nullfields') {
+      response.contentType('application/json; charset=UTF-8')
+      response.status(201)
+      response.headers.add('HEADER-X', 'Y')
+      render('''
+        [
+            {
+                "documentId": 0,
+                "documentCategoryId": 5,
+                "documentCategoryCode": null,
+                "contentLength": 0,
+                "tags": null
+            },
+            {
+                "documentId": 1,
+                "documentCategoryId": 5,
+                "documentCategoryCode": null,
+                "contentLength": 0,
+                "tags": null
+            }
+        ]
+      ''')
+    }
+
+    files { dir "public" }
+  }
+}
