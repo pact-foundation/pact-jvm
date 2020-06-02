97dbcad9f620817afd8e32c92ab9d22f815c3085
Christoph Portmann <christoph.portmann@thoughtworks.com>
2020-06-02 10:16:56 +0200

    add helper function for MessagePacts
    the helper function return the verification result and write the pact to a file

--- consumer/src/main/kotlin/au/com/dius/pact/consumer/ConsumerPactRunner.kt
+++ consumer/src/main/kotlin/au/com/dius/pact/consumer/ConsumerPactRunner.kt
@@ -1,7 +1,10 @@
 package au.com.dius.pact.consumer
 
 import au.com.dius.pact.consumer.model.MockProviderConfig
+import au.com.dius.pact.core.model.PactSpecVersion
 import au.com.dius.pact.core.model.RequestResponsePact
+import au.com.dius.pact.core.model.messaging.Message
+import au.com.dius.pact.core.model.messaging.MessagePact
 
 interface PactTestRun<R> {
   @Throws(Throwable::class)
@@ -11,4 +14,24 @@ interface PactTestRun<R> {
 fun <R> runConsumerTest(pact: RequestResponsePact, config: MockProviderConfig, test: PactTestRun<R>): PactVerificationResult {
   val server = mockServer(pact, config)
   return server.runAndWritePact(pact, config.pactVersion, test)
+}
+
+interface MessagePactTestRun<R> {
+  @Throws(Throwable::class)
+  fun run(messages: List<Message>, context: PactTestExecutionContext?): R
+}
+
+fun <R> runMessageConsumerTest(
+  pact: MessagePact,
+  pactVersion: PactSpecVersion = PactSpecVersion.V3,
+  testFunc: MessagePactTestRun<R>
+): PactVerificationResult {
+  return try {
+    val context = PactTestExecutionContext()
+    val result = testFunc.run(pact.messages, context)
+    pact.write(context.pactFolder, pactVersion)
+    PactVerificationResult.Ok(result)
+  } catch (e: Throwable) {
+    PactVerificationResult.Error(e, PactVerificationResult.Ok())
+  }
 }
