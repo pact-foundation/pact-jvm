73f30e0052850401cfe827eee84d35d8c11bff46
Ronald Holshausen <uglyog@dius.com.au>
2020-07-10 10:27:28 +1000

    fix: codenarc errors

--- provider/junit5/src/test/groovy/au/com/dius/pact/provider/junit5/PactVerificationExtensionSpec.groovy
+++ provider/junit5/src/test/groovy/au/com/dius/pact/provider/junit5/PactVerificationExtensionSpec.groovy
@@ -2,7 +2,6 @@ package au.com.dius.pact.provider.junit5
 
 import au.com.dius.pact.core.model.Consumer
 import au.com.dius.pact.core.model.FilteredPact
-import au.com.dius.pact.core.model.Interaction
 import au.com.dius.pact.core.model.PactBrokerSource
 import au.com.dius.pact.core.model.Provider
 import au.com.dius.pact.core.model.Request
@@ -10,7 +9,9 @@ import au.com.dius.pact.core.model.RequestResponseInteraction
 import au.com.dius.pact.core.model.RequestResponsePact
 import au.com.dius.pact.core.model.Response
 import au.com.dius.pact.core.support.expressions.ValueResolver
-import au.com.dius.pact.provider.*
+import au.com.dius.pact.provider.IConsumerInfo
+import au.com.dius.pact.provider.IProviderInfo
+import au.com.dius.pact.provider.IProviderVerifier
 import org.junit.jupiter.api.extension.ExtensionContext
 import spock.lang.Specification
 
@@ -30,17 +31,17 @@ class PactVerificationExtensionSpec extends Specification {
       getStore(_) >> store
     }
 
-
     def interaction1 = new RequestResponseInteraction('interaction1', [], new Request(), new Response())
     def interaction2 = new RequestResponseInteraction('interaction2', [], new Request(), new Response())
     def pact = new RequestResponsePact(new Provider(), new Consumer(), [interaction1, interaction2 ])
     def filteredPact = new FilteredPact(pact, { it.description == 'interaction1' })
     PactBrokerSource pactSource = new PactBrokerSource('localhost', '80', 'http')
 
-    context = new PactVerificationContext(store, extContext, Stub(TestTarget), Stub(IProviderVerifier), Stub(ValueResolver),
-            Stub(IProviderInfo), Stub(IConsumerInfo), interaction1, [])
+    context = new PactVerificationContext(store, extContext, Stub(TestTarget), Stub(IProviderVerifier),
+      Stub(ValueResolver), Stub(IProviderInfo), Stub(IConsumerInfo), interaction1, [])
 
-    PactVerificationExtension extension = new PactVerificationExtension(filteredPact, pactSource, interaction1, "service", "consumer")
+    PactVerificationExtension extension = new PactVerificationExtension(filteredPact, pactSource, interaction1,
+      'service', 'consumer')
 
     when:
     extension.afterTestExecution(extContext)
@@ -63,16 +64,16 @@ class PactVerificationExtensionSpec extends Specification {
             getStore(_) >> store
         }
 
-
         def interaction1 = new RequestResponseInteraction('interaction1', [], new Request(), new Response())
         def interaction2 = new RequestResponseInteraction('interaction2', [], new Request(), new Response())
         def pact = new RequestResponsePact(new Provider(), new Consumer(), [interaction1, interaction2 ])
         PactBrokerSource pactSource = new PactBrokerSource('localhost', '80', 'http')
 
-        context = new PactVerificationContext(store, extContext, Stub(TestTarget), Stub(IProviderVerifier), Stub(ValueResolver),
-                Stub(IProviderInfo), Stub(IConsumerInfo), interaction1, [])
+        context = new PactVerificationContext(store, extContext, Stub(TestTarget), Stub(IProviderVerifier),
+          Stub(ValueResolver), Stub(IProviderInfo), Stub(IConsumerInfo), interaction1, [])
 
-        PactVerificationExtension extension = new PactVerificationExtension(pact, pactSource, interaction1, "service", "consumer")
+        PactVerificationExtension extension = new PactVerificationExtension(pact, pactSource, interaction1,
+          'service', 'consumer')
 
         when:
         extension.afterTestExecution(extContext)
