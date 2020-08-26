b6e81b71a1d9280299c2c807e5cc24fd839d5993
Mark Abrahams <mpabrahams@gmail.com>
2020-08-26 13:50:08 +0200

    fixed code formatting

--- pact-jvm-server/src/test/groovy/au/com/dius/pact/server/PublishSpec.groovy
+++ pact-jvm-server/src/test/groovy/au/com/dius/pact/server/PublishSpec.groovy
@@ -1,7 +1,5 @@
 package au.com.dius.pact.server
 
-import au.com.dius.pact.server.Config
-import au.com.dius.pact.server.Publish
 import spock.lang.Specification
 
 class PublishSpec extends Specification {
@@ -15,25 +13,37 @@ class PublishSpec extends Specification {
         def result = Publish.getBrokerUrlFromConfig(config)
 
         then:
-        !result.isDefined()
+        !result.defined
     }
 
     def 'valid broker url will set broker'() {
         given:
-        def config = new Config(80, '0.0.0.0', false, 100, 200, false, 3, '', '', 0, 'https://valid.broker.com', 'abc#3')
+        def config = new Config(80,
+                '0.0.0.0',
+                false,
+                100,
+                200,
+                false,
+                3,
+                '',
+                '',
+                0,
+                'https://valid.broker.com',
+                'abc#3'
+        )
 
         when:
         def result = Publish.getBrokerUrlFromConfig(config)
 
         then:
-        result.isDefined()
+        result.defined
     }
 
     def 'successful read on valid file'() {
         given:
-        def content = """ {"consumer": "testconsumer", "provider": "testprovider"} """
-        def fileName = "test.json"
-        def rootDir = System.getProperty("pact.rootDir", "target/pacts")
+        def content = '{"consumer": "testconsumer", "provider": "testprovider"}'
+        def fileName = 'test.json'
+        def rootDir = System.getProperty('pact.rootDir', 'target/pacts')
         def file = new File("${rootDir}/$fileName")
         new File(rootDir).mkdirs()
         file.write(content)
@@ -45,12 +55,12 @@ class PublishSpec extends Specification {
         content == result
 
         cleanup:
-        new File(System.getProperty("pact.rootDir", "target")).deleteDir()
+        new File(System.getProperty('pact.rootDir', 'target')).deleteDir()
     }
 
     def 'unsuccessful read on invalid file'() {
         when:
-        Publish.readContract("invalid")
+        Publish.readContract('invalid')
 
         then:
         thrown(IOException)
