4259e569449a8f60fea621c385615a0a23af5d29
Ronald Holshausen <uglyog@dius.com.au>
2020-09-20 14:46:21 +1000

    fix: correct regex in test

--- core/model/src/test/groovy/au/com/dius/pact/core/model/generators/RandomDecimalGeneratorSpec.groovy
+++ core/model/src/test/groovy/au/com/dius/pact/core/model/generators/RandomDecimalGeneratorSpec.groovy
@@ -23,11 +23,11 @@ class RandomDecimalGeneratorSpec extends Specification {
 
   def 'handle edge case when digits == 1'() {
     expect:
-    new RandomDecimalGenerator(1).generate([:]).toString() ==~ /^\d/
+    new RandomDecimalGenerator(1).generate([:]).toString() ==~ /^\d$/
   }
 
   def 'handle edge case when digits == 2'() {
     expect:
-    new RandomDecimalGenerator(2).generate([:]).toString() ==~ /^\d\.\d/
+    new RandomDecimalGenerator(2).generate([:]).toString() ==~ /^\d\.\d$/
   }
 }
