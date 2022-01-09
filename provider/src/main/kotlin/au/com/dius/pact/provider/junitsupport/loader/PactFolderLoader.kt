b48e37daf6d3171fec7c402ad8fc65dcfd21e54a
Francisco González Ibáñez <f.gonzalez@lookiero.com>
2022-01-09 11:50:44 +0100

    feat: Add support for PactLoader path value expressions

--- provider/src/main/kotlin/au/com/dius/pact/provider/junitsupport/loader/PactFolderLoader.kt
+++ provider/src/main/kotlin/au/com/dius/pact/provider/junitsupport/loader/PactFolderLoader.kt
@@ -4,19 +4,44 @@ import au.com.dius.pact.core.model.DefaultPactReader
 import au.com.dius.pact.core.model.DirectorySource
 import au.com.dius.pact.core.model.Interaction
 import au.com.dius.pact.core.model.Pact
+import au.com.dius.pact.core.support.expressions.DataType
+import au.com.dius.pact.core.support.expressions.ExpressionParser
+import au.com.dius.pact.core.support.expressions.SystemPropertyResolver
+import au.com.dius.pact.core.support.expressions.ValueResolver
 import java.io.File
 import java.net.URLDecoder
+import kotlin.reflect.KClass
 
 /**
  * Out-of-the-box implementation of [PactLoader]
  * that loads pacts from either a subfolder of project resource folder or a directory
  */
-class PactFolderLoader<I>(private val path: File) : PactLoader where I : Interaction {
-  private val pactSource: DirectorySource<I> = DirectorySource(path)
+class PactFolderLoader<I> : PactLoader where I : Interaction {
 
-  constructor(path: String) : this(File(path))
+  private val path: File
+  private val pactSource: DirectorySource<I>
 
-  constructor(pactFolder: PactFolder) : this(pactFolder.value)
+  @JvmOverloads
+  constructor(
+    path: String,
+    valueResolverClass: KClass<out ValueResolver>? = null,
+    valueResolver: ValueResolver? = null
+  ) {
+    val resolver = setupValueResolver(valueResolver, valueResolverClass)
+    val interpolatedPath = ExpressionParser.parseExpression(path, DataType.STRING, resolver) as String
+    this.path = File(interpolatedPath)
+    this.pactSource = DirectorySource(this.path)
+  }
+
+  constructor(pactFolder: PactFolder) : this(
+    pactFolder.value,
+    pactFolder.valueResolver
+  )
+
+  constructor(path: File) {
+    this.path = path
+    this.pactSource = DirectorySource(this.path)
+  }
 
   override fun description() = "Directory(${pactSource.dir})"
 
@@ -45,5 +70,26 @@ class PactFolderLoader<I>(private val path: File) : PactLoader where I : Interac
     } else {
       return path
     }
+  }
+
+  private fun setupValueResolver(
+    valueResolver: ValueResolver?,
+    valueResolverClass: KClass<out ValueResolver>?
+  ): ValueResolver {
+    var resolver: ValueResolver = valueResolver ?: SystemPropertyResolver
+    if (valueResolverClass != null) {
+      if (valueResolverClass.objectInstance != null) {
+        resolver = valueResolverClass.objectInstance!!
+      } else {
+        try {
+          resolver = valueResolverClass.java.newInstance()
+        } catch (e: InstantiationException) {
+          PactBrokerLoader.logger.warn(e) { "Failed to instantiate the value resolver, using the default" }
+        } catch (e: IllegalAccessException) {
+          PactBrokerLoader.logger.warn(e) { "Failed to instantiate the value resolver, using the default" }
+        }
+      }
+    }
+    return resolver
   }
 }
