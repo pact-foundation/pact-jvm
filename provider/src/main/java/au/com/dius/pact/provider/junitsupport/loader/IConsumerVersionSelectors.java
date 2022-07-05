471c13e8f921f1f5e28c51f9516018f30ab91aae
Ronald Holshausen <ronald.holshausen@gmail.com>
2022-07-05 15:42:29 +1000

    refactor: Consumer Version Selectors method does not need a parameter

--- provider/src/main/java/au/com/dius/pact/provider/junitsupport/loader/IConsumerVersionSelectors.java
+++ provider/src/main/java/au/com/dius/pact/provider/junitsupport/loader/IConsumerVersionSelectors.java
@@ -8,5 +8,5 @@ public interface IConsumerVersionSelectors {
      * Return the consumer version selectors to use in the test
      */
     @au.com.dius.pact.provider.junitsupport.loader.ConsumerVersionSelectors
-    SelectorBuilder consumerVersionSelectors(SelectorBuilder builder);
+    SelectorBuilder consumerVersionSelectors();
 }
