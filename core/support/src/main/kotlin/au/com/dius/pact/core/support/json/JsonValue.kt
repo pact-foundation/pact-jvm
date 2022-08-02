03aaec2b1d06bc9fbe2dbeb8cbc54510523dec91
Ronald Holshausen <ronald.holshausen@gmail.com>
2022-08-02 16:54:38 +1000

    chore: fix code check after merging from master

--- core/support/src/main/kotlin/au/com/dius/pact/core/support/json/JsonValue.kt
+++ core/support/src/main/kotlin/au/com/dius/pact/core/support/json/JsonValue.kt
@@ -116,7 +116,12 @@ sealed class JsonValue {
       is True -> "true"
       is False -> "false"
       is Array -> "[${this.values.joinToString(",") { it.serialise() }}]"
-      is Object -> "{${this.entries.entries.sortedBy { it.key }.joinToString(",") { "\"${it.key}\":" + it.value.serialise() }}}"
+      is Object -> {
+        val s = this.entries.entries
+          .sortedBy { it.key }
+          .joinToString(",") { "\"${it.key}\":" + it.value.serialise() }
+        "{$s}"
+      }
     }
   }
 
