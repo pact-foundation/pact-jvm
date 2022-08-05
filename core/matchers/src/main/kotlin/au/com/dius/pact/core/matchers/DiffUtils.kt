18832534bd6bbd254f297262666a8314073ddd53
Bernard Fugas <bfugas@gmail.com>
2022-08-05 08:38:11 +0200

    Replace diff-utils with the latest actively maintained version 4.12

--- core/matchers/src/main/kotlin/au/com/dius/pact/core/matchers/DiffUtils.kt
+++ core/matchers/src/main/kotlin/au/com/dius/pact/core/matchers/DiffUtils.kt
@@ -1,33 +1,34 @@
 package au.com.dius.pact.core.matchers
 
 import au.com.dius.pact.core.support.json.JsonValue
-import difflib.ChangeDelta
+import com.github.difflib.DiffUtils
+import com.github.difflib.patch.ChangeDelta
 
 private const val NEW_LINE = '\n'
 
 fun generateDiff(expectedBodyString: String, actualBodyString: String): List<String> {
   val expectedLines = expectedBodyString.split(NEW_LINE)
   val actualLines = actualBodyString.split(NEW_LINE)
-  val patch = difflib.DiffUtils.diff(expectedLines, actualLines)
+  val patch = DiffUtils.diff(expectedLines, actualLines)
 
   val diff = mutableListOf<String>()
   var line = 0
   patch.deltas.forEach { delta ->
     when (delta) {
       is ChangeDelta<*> -> {
-        if (delta.original.position >= 1 && (diff.isEmpty() ||
-            expectedLines[delta.original.position - 1] != diff.last())) {
-          diff.addAll(expectedLines.slice(line until delta.original.position))
+        if (delta.source.position >= 1 && (diff.isEmpty() ||
+            expectedLines[delta.source.position - 1] != diff.last())) {
+          diff.addAll(expectedLines.slice(line until delta.source.position))
         }
 
-        delta.original.lines.forEach {
+        delta.source.lines.forEach {
           diff.add("-$it")
         }
-        delta.revised.lines.forEach {
+        delta.target.lines.forEach {
           diff.add("+$it")
         }
 
-        line = delta.original.position + delta.original.lines.size
+        line = delta.source.position + delta.source.lines.size
       }
     }
   }
