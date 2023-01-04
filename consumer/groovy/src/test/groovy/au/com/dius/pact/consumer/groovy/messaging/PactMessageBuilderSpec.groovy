09bb4afe3a66317e6b52564262b0e96e5f2fcb41
Ronald Holshausen <ronald.holshausen@smartbear.com>
2023-01-04 11:29:54 +1100

    feat: Update Groovy message builder to be able to create a message with a NULL body #1637

--- consumer/groovy/src/test/groovy/au/com/dius/pact/consumer/groovy/messaging/PactMessageBuilderSpec.groovy
+++ consumer/groovy/src/test/groovy/au/com/dius/pact/consumer/groovy/messaging/PactMessageBuilderSpec.groovy
@@ -149,4 +149,57 @@ class PactMessageBuilderSpec extends Specification {
       assert message.jsonContents
     }
   }
+
+  def 'receiving a message with a NULL body'() {
+    given:
+    builder {
+      expectsToReceive 'a confirmation delete message'
+      withMetadata(contentType: 'application/json', messageId: '12345678')
+      withContent(null)
+    }
+
+    when:
+    builder.run { Message message ->
+      def content = new JsonSlurper().parse(message.contentsAsBytes())
+      assert content == null
+    }
+
+    then:
+    true
+  }
+
+  def 'receiving a message with an empty body'() {
+    given:
+    builder {
+      expectsToReceive 'a confirmation delete message'
+      withMetadata(contentType: 'application/json', messageId: '12345678')
+      withContent { }
+    }
+
+    when:
+    builder.run { Message message ->
+      def content = new JsonSlurper().parse(message.contentsAsBytes())
+      assert content.size() == 0
+    }
+
+    then:
+    true
+  }
+
+  def 'receiving a message with a missing body'() {
+    given:
+    builder {
+      expectsToReceive 'a confirmation delete message'
+      withMetadata(messageId: '12345678')
+    }
+
+    when:
+    builder.run { Message message ->
+      assert message.contentsAsBytes().size() == 0
+      assert message.metadata.messageId == '12345678'
+    }
+
+    then:
+    true
+  }
 }
