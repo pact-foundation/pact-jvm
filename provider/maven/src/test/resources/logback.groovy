8c9c03aa5334e4e91703f356be18adfce7fdb41a
Ronald Holshausen <ronald.holshausen@smartbear.com>
2023-08-15 15:34:55 +1000

    Squashed 'compatibility-suite/pact-compatibility-suite/' changes from 88a21014d..4db89a19c
    4db89a19c fix: Make empty body explicit
    488e7504f feat: update V4 mismatch errors
    
    git-subtree-dir: compatibility-suite/pact-compatibility-suite
    git-subtree-split: 4db89a19ca06854febafeb56ff7007c434095c29

--- provider/maven/src/test/resources/logback.groovy
+++
@@ -1,8 +0,0 @@
-import ch.qos.logback.classic.encoder.PatternLayoutEncoder
-
-appender("STDOUT", ConsoleAppender) {
-  encoder(PatternLayoutEncoder) {
-    pattern = "%d{HH:mm:ss.SSS} [%thread] %-5level %logger{36} - %msg%n"
-  }
-}
-root(DEBUG, ["STDOUT"])
--- provider/src/test/groovy/au/com/dius/pact/provider/TestVerifyResponseByInvokingProviderMethodsClass.groovy
+++
@@ -1,8 +0,0 @@
-package au.com.dius.pact.provider
-
-import org.apache.commons.lang3.NotImplementedException
-
-class TestVerifyResponseByInvokingProviderMethodsClass {
-  @PactVerifyProvider('verifyResponseByInvokingProviderMethods Test Message')
-  String method() { throw new NotImplementedException('Boom!') }
-}
