fe8e0cc69fa9615089656fea519231959ce49af6
Ronald Holshausen <ronald.holshausen@smartbear.com>
2023-12-18 11:26:28 +1100

    chore: Correct Javadoc for @Pact annotation #1739

--- core/model/src/main/kotlin/au/com/dius/pact/core/model/annotations/Pact.kt
+++ core/model/src/main/kotlin/au/com/dius/pact/core/model/annotations/Pact.kt
@@ -2,9 +2,16 @@ package au.com.dius.pact.core.model.annotations
 
 /**
  * describes the interactions between a provider and a consumer used in JUnit tests.
- * The annotated method has to be of following signature:
+ * The annotated method has to be one of following signatures:
  *
+ * For legacy DSL classes and request/response interactions:
  * public RequestResponsePact providerDef1(PactDslWithProvider builder) {...}
+ *
+ * For message interactions:
+ * public MessagePact providerDef1(MessagePactBuilder builder)
+ *
+ * For V4 DSL classes and any interaction type:
+ * public V4Pact providerDef1(PactBuilder builder) {...}
  *
  * @author pmucha
  */
