0122422468d016b481dd8bba3298502cfab16a0f
Ronald Holshausen <ronald.holshausen@smartbear.com>
2025-11-10 14:35:19 +1100

    chore: Update provider dependencies

--- provider/spring/src/test/java/au/com/dius/pact/provider/spring/BooksPactProviderTest.java
+++ provider/spring/src/test/java/au/com/dius/pact/provider/spring/BooksPactProviderTest.java
@@ -27,7 +27,7 @@ import java.util.ArrayList;
 import java.util.List;
 import java.util.UUID;
 
-import static org.mockito.Matchers.any;
+import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.Mockito.when;
 
 @RunWith(RestPactRunner.class)
